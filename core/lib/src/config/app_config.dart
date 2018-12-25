58efba706942126566721c01cef7ab85a7e26880
zhanhongbo1112 <eric.zhan@163.com>
2018-12-25 18:09:07 +0800

    path provider

--- core/lib/src/config/app_config.dart
+++ core/lib/src/config/app_config.dart
@@ -5,4 +5,7 @@ class AppConfig {
   static const BASE_URL = 'https://localhost:9002/djwebservices/v2';
   static const ACCESS_TOKEN_KEY = 'access_token';
   static const BASIC_AUTH_TOKEN_KEY = 'basic_auth_token';
+
+  /// 应用名称（英文），用于获取应用程序目录
+  static const APP_NAME = 'nbyjy';
 }
---
+++ core/lib/src/context/local_path.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import '../config/app_config.dart';
+
+class LocalPath {
+  static getLocalPath() async {
+    Directory appDir;
+    if (Platform.isIOS) {
+      appDir = await getApplicationDocumentsDirectory();
+    } else {
+      appDir = await getExternalStorageDirectory();
+    }
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    if (permission != PermissionStatus.granted) {
+      Map<PermissionGroup, PermissionStatus> permissions =
+          await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      if (permissions[PermissionGroup.storage] != PermissionStatus.granted) {
+        return null;
+      }
+    }
+    String appDocPath = appDir.path + "/" + AppConfig.APP_NAME;
+    Directory appPath = Directory(appDocPath);
+    await appPath.create(recursive: true);
+    return appPath;
+  }
+}
