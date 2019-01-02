e37fc150c6ef65829efd56b8dbf53caa3ba3ec93
Konoha-orz <517136675@qq.com>
2019-01-02 15:29:45 +0800

    fluwx引入，b2bcommerce android调整，添加签名

---
+++ b2b_commerce/android/app/src/main/java/com/example/b2bcommerce/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.b2bcommerce;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+
+
+}
