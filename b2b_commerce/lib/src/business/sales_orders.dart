0ca3144940a671232fe5a7941c9df34852c2830e
hejing <jing.he@dingjust.com>
2019-01-07 00:36:26 +0800

    销售订单还原

--- b2b_commerce/lib/src/business/sales_orders.dart
+++ b2b_commerce/lib/src/business/sales_orders.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'orders/sales_order_item.dart';
 
 class SalesOrdersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('销售订单'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('销售订单'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Text('全部')),
+              Tab(icon: Text('待付款')),
+              Tab(icon: Text('待发货')),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            SaleOrderItem(),
+            Center(
+              child: Text('待付款订单'),
+            ),
+            Center(
+              child: Text('待发货订单'),
+            )
+          ],
+        ),
       ),
-      body: Text('TODO: 销售订单'),
     );
   }
 }
