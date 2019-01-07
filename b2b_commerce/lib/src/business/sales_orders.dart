a9e52691427fe5c3f2b8f8e540cec03568e5d2a9
hejing <jing.he@dingjust.com>
2019-01-07 10:52:17 +0800

    销售订单样式修改

--- b2b_commerce/lib/src/business/sales_orders.dart
+++ b2b_commerce/lib/src/business/sales_orders.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
 import 'orders/sales_order_item.dart';
 
+final List<Tab> status = <Tab>[
+  new Tab(text: '全部'),
+  new Tab(text: '待付款'),
+  new Tab(text: '待发货'),
+  new Tab(text: '已发货'),
+  new Tab(text: '已完成'),
+];
+
 class SalesOrdersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('销售订单'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Text('全部')),
-              Tab(icon: Text('待付款')),
-              Tab(icon: Text('待发货')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('销售订单'),
+      ),
+      body: DefaultTabController(
+        length: status.length,
+        child: Scaffold(
+          appBar: TabBar(
+            unselectedLabelColor: Colors.black26,
+            labelColor: Colors.black38,
+            tabs: status,
+            labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Colors.black
+            ),
+            isScrollable: true,
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              SaleOrderItem(),
+              Center(
+                child: Text('待付款订单'),
+              ),
+              Center(
+                child: Text('待发货订单'),
+              ),
+              Center(
+                child: Text('已发货订单'),
+              ),
+              Center(
+                child: Text('已完成订单'),
+              ),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            SaleOrderItem(),
-            Center(
-              child: Text('待付款订单'),
-            ),
-            Center(
-              child: Text('待发货订单'),
-            )
-          ],
         ),
       ),
     );
