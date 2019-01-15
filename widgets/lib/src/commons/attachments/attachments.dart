1aec2e287d3a6d104fe9e1bec16d95a4891259d0
Konoha-orz <517136675@qq.com>
2019-01-15 14:09:17 +0800

    附件组件优化1.新增上下一张图片功能2.新增点击放大预览功能

--- widgets/lib/src/commons/attachments/attachments.dart
+++ widgets/lib/src/commons/attachments/attachments.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 //横向滚动图片列表
-class Attachments extends StatelessWidget {
-  const Attachments(
+class Attachments extends StatefulWidget {
+  Attachments(
       {Key key,
       @required this.list,
       this.width = 320,
@@ -18,45 +18,116 @@ class Attachments extends StatelessWidget {
   final double imageWidth;
   final double imageHeight;
 
+  _AttachmentsState createState() => _AttachmentsState();
+}
+
+class _AttachmentsState extends State<Attachments> {
+  ScrollController _scrollController = new ScrollController();
+  Color iconColorLeft = Colors.grey[200];
+  Color iconColorRight = Colors.black;
+
   @override
   Widget build(BuildContext context) {
+    //监听到底
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        setState(() {
+          iconColorRight = Colors.grey[200];
+        });
+      } else if (_scrollController.position.pixels == 0) {
+        //监听开头
+        setState(() {
+          iconColorLeft = Colors.grey[200];
+        });
+      } else {
+        setState(() {
+          iconColorLeft = Colors.black;
+          iconColorRight = Colors.black;
+        });
+      }
+    });
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Icon(Icons.chevron_left, color: Colors.grey),
-        Expanded(
-          child: _buildAttachmentsListVie(width),
+        IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: iconColorLeft,
+          ),
+          tooltip: '上一张',
+          onPressed: () {
+            _scrollController.animateTo(_scrollController.position.pixels - 100,
+                duration: Duration(milliseconds: 200), curve: Curves.ease);
+          },
         ),
-        Icon(Icons.chevron_right, color: Colors.grey)
+        Expanded(
+          child: _buildAttachmentsListVie(widget.width, context),
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.chevron_right,
+            color: iconColorRight,
+          ),
+          tooltip: '下一张',
+          onPressed: () {
+            _scrollController.animateTo(_scrollController.position.pixels + 100,
+                duration: Duration(milliseconds: 200), curve: Curves.ease);
+          },
+        ),
       ],
     );
   }
 
-  Widget _buildAttachmentsListVie(double width) {
+  Widget _buildAttachmentsListVie(double width, BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
-      height: height,
+      height: widget.height,
       width: width,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: list
+        controller: _scrollController,
+        children: widget.list
             .map(
-              (url) => Container(
-                    width: imageWidth,
-                    height: imageHeight,
-                    margin: EdgeInsets.symmetric(horizontal: 5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
-                      image: DecorationImage(
-                        image: NetworkImage(url),
-                        fit: BoxFit.cover,
+              (url) => GestureDetector(
+                    child: Container(
+                      width: widget.imageWidth,
+                      height: widget.imageHeight,
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey,
+                        image: DecorationImage(
+                          image: NetworkImage(url),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
+                    onTap: () {
+                      onPreview(context, url);
+                    },
                   ),
             )
             .toList(),
       ),
+    );
+  }
+
+  void onPreview(BuildContext context, String url) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          children: <Widget>[
+            Image.network(
+              url,
+              fit: BoxFit.cover,
+            )
+          ],
+          contentPadding: EdgeInsets.all(0),
+        );
+      },
     );
   }
 }
