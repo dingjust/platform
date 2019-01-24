8a81ce44341c9f25341a6ee742c8f7f5d292155d
Konoha-orz <517136675@qq.com>
2019-01-24 11:20:24 +0800

    快反工厂

--- b2b_commerce/lib/src/home/factory/factory.dart
+++ b2b_commerce/lib/src/home/factory/factory.dart
@@ -1,14 +1,20 @@
-
 import 'package:flutter/material.dart';
 import 'package:models/models.dart';
 import 'package:widgets/widgets.dart';
 
 class FactoryItem extends StatelessWidget {
-  const FactoryItem({Key key, @required this.model, this.showButton = false})
+  const FactoryItem(
+      {Key key,
+      @required this.model,
+      this.showButton = false,
+      this.hasInvited = false})
       : super(key: key);
 
   final FactoryModel model;
+  ///是否显示按钮
   final bool showButton;
+  ///是否已经邀请
+  final bool hasInvited;
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +29,8 @@ class FactoryItem extends StatelessWidget {
           children: <Widget>[
             Container(
               margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              width: 100,
-              height: 100,
+              width: 80,
+              height: 80,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   image: DecorationImage(
@@ -52,21 +58,53 @@ class FactoryItem extends StatelessWidget {
                 ],
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Stars(
-                      size: 15,
-                      color: Color.fromRGBO(255, 183, 0, 1),
-                      highlightOnly: false,
-                      starLevel: model.starLevel,
-                    )
-                  ],
-                )
-              ],
-            ),
+            Container(
+              width: 280,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Stars(
+                        size: 15,
+                        color: Color.fromRGBO(255, 183, 0, 1),
+                        highlightOnly: false,
+                        starLevel: model.starLevel,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: _buildCategoryTags(),
+                      )
+                    ],
+                  ),
+                  Container(
+                    child: Text(
+                      model.describe,
+                      overflow: TextOverflow.clip,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                            text: '历史接单',
+                            style: TextStyle(color: Colors.grey),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: '${model.historyOrdersCount}',
+                                  style: TextStyle(color: Colors.red)),
+                              TextSpan(text: '单')
+                            ]),
+                      ),
+                      _buildButton()
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
         decoration: BoxDecoration(
@@ -74,6 +112,88 @@ class FactoryItem extends StatelessWidget {
             border: Border(
                 bottom: BorderSide(color: Colors.grey[300], width: 0.6))),
       ),
+    );
+  }
+
+  Widget _buildCategoryTags() {
+    //取前3条
+    var tags = <CategoryTag>[];
+    for (int i = 0; i < model.categories.length && i < 3; i++) {
+      tags.add(CategoryTag(label: model.categories[i].name));
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: tags,
+    );
+  }
+
+  Widget _buildButton() {
+    if (showButton) {
+      if (hasInvited) {
+        return Container(
+          height: 23,
+          width: 80,
+          child: Center(
+            child: Text(
+              '已邀请',
+              style: TextStyle(color: Colors.white, fontSize: 12),
+            ),
+          ),
+          decoration: BoxDecoration(
+              color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+        );
+      } else {
+        return Container(
+          height: 23,
+          width: 80,
+          child: FlatButton(
+            onPressed: () {},
+            color: Colors.orange,
+            child: Text(
+              '邀请报价',
+              style: TextStyle(color: Colors.white, fontSize: 12),
+            ),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          ),
+        );
+      }
+    } else {
+      return Container();
+    }
+  }
+}
+
+class CategoryTag extends StatelessWidget {
+  const CategoryTag(
+      {Key key,
+      this.width = 32,
+      this.height = 18,
+      @required this.label,
+      this.color = const Color.fromRGBO(244, 143, 177, 1.0),
+      this.backgroundColor = const Color.fromRGBO(248, 187, 208, 0.3)})
+      : super(key: key);
+
+  final double width;
+  final double height;
+  final String label;
+  final Color color;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(2, 0, 2, 0),
+      height: height,
+      margin: EdgeInsets.symmetric(horizontal: 2),
+      child: Center(
+        child: Text(
+          label,
+          style: TextStyle(color: color, fontSize: 12),
+        ),
+      ),
+      decoration: BoxDecoration(
+          color: backgroundColor, borderRadius: BorderRadius.circular(2)),
     );
   }
 }
--- services/lib/src/home/bloc/quick_reaction_factory_bloc.dart
+++ services/lib/src/home/bloc/quick_reaction_factory_bloc.dart
@@ -21,7 +21,7 @@ class QuickReactionFactoryBLoC extends BLoCBase {
     return _instance;
   }
 
-  static final List<FactoryModel> _factories = [];
+  List<FactoryModel> _factories = [];
 
   List<FactoryModel> get factories => _factories;
 
@@ -39,56 +39,92 @@ class QuickReactionFactoryBLoC extends BLoCBase {
     //若没有数据则查询
     if (_factories.isEmpty) {
       // TODO: 分页拿数据，response.data;
-      _factories.addAll(await Future.delayed(const Duration(seconds: 1), () {
+      _factories = (await Future.delayed(const Duration(seconds: 1), () {
         if (isDESC) {
           return <FactoryModel>[
             FactoryModel(
                 historyOrdersCount: 214,
                 name: '广州旭日4',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
                 ]),
             FactoryModel(
                 historyOrdersCount: 214,
                 name: '广州旭日3',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
                 ]),
             FactoryModel(
                 historyOrdersCount: 214,
                 name: '广州旭日2',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
                 ]),
             FactoryModel(
                 historyOrdersCount: 214,
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 name: '广州旭日1',
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 starLevel: 4,
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
+                ]),
+            FactoryModel(
+                historyOrdersCount: 214,
+                profilePicture:
+                    'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
+                name: '广州旭日0',
+                starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
+                categories: [
+                  CategoryModel(name: '牛仔'),
+                  CategoryModel(name: '衬衫'),
+                  CategoryModel(name: '夹克'),
+                ]),
+            FactoryModel(
+                historyOrdersCount: 214,
+                profilePicture:
+                    'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
+                name: '广州旭日-1',
+                starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
+                categories: [
+                  CategoryModel(name: '牛仔'),
+                  CategoryModel(name: '衬衫'),
+                  CategoryModel(name: '夹克'),
+                ]),
+            FactoryModel(
+                historyOrdersCount: 214,
+                profilePicture:
+                    'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
+                name: '广州旭日-2',
+                starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
+                categories: [
+                  CategoryModel(name: '牛仔'),
+                  CategoryModel(name: '衬衫'),
+                  CategoryModel(name: '夹克'),
                 ])
           ];
         } else {
@@ -97,49 +133,85 @@ class QuickReactionFactoryBLoC extends BLoCBase {
                 historyOrdersCount: 214,
                 name: '广州旭日1',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
                 ]),
             FactoryModel(
                 historyOrdersCount: 214,
                 name: '广州旭日2',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
                 ]),
             FactoryModel(
                 historyOrdersCount: 214,
                 name: '广州旭日3',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
                 ]),
             FactoryModel(
                 historyOrdersCount: 214,
                 name: '广州旭日4',
                 starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
                 profilePicture:
                     'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
                 categories: [
                   CategoryModel(name: '牛仔'),
                   CategoryModel(name: '衬衫'),
                   CategoryModel(name: '夹克'),
-                  CategoryModel(name: '卫衣'),
+                ]),
+            FactoryModel(
+                historyOrdersCount: 214,
+                profilePicture:
+                    'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
+                name: '广州旭日5',
+                starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
+                categories: [
+                  CategoryModel(name: '牛仔'),
+                  CategoryModel(name: '衬衫'),
+                  CategoryModel(name: '夹克'),
+                ]),
+            FactoryModel(
+                historyOrdersCount: 214,
+                profilePicture:
+                    'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
+                name: '广州旭日6',
+                starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
+                categories: [
+                  CategoryModel(name: '牛仔'),
+                  CategoryModel(name: '衬衫'),
+                  CategoryModel(name: '夹克'),
+                ]),
+            FactoryModel(
+                historyOrdersCount: 214,
+                profilePicture:
+                    'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
+                name: '广州旭日7',
+                starLevel: 4,
+                describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
+                categories: [
+                  CategoryModel(name: '牛仔'),
+                  CategoryModel(name: '衬衫'),
+                  CategoryModel(name: '夹克'),
                 ])
           ];
         }
@@ -150,19 +222,19 @@ class QuickReactionFactoryBLoC extends BLoCBase {
 
   loadingMoreByCondition({String condition, bool isDESC}) async {
     //模拟数据到底
-    if (_factories.length < 6) {
+    if (_factories.length < 10) {
       _factories.add(await Future.delayed(const Duration(seconds: 1), () {
         return FactoryModel(
             historyOrdersCount: 214,
             name: '广州旭日',
             starLevel: 4,
+            describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
             profilePicture:
                 'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
             categories: [
               CategoryModel(name: '牛仔'),
               CategoryModel(name: '衬衫'),
               CategoryModel(name: '夹克'),
-              CategoryModel(name: '卫衣'),
             ]);
       }));
     } else {
@@ -187,13 +259,13 @@ class QuickReactionFactoryBLoC extends BLoCBase {
           historyOrdersCount: 214,
           name: '广州旭日',
           starLevel: 4,
+          describe: '20年经验专业生产牛仔服装，价低质优，本月剩余小量空闲产能，欢迎报价',
           profilePicture:
               'https://img.alicdn.com/imgextra/i2/50540166/TB2RBoYahOGJuJjSZFhXXav4VXa_!!0-saturn_solar.jpg_220x220.jpg_.webp',
           categories: [
             CategoryModel(name: '牛仔'),
             CategoryModel(name: '衬衫'),
             CategoryModel(name: '夹克'),
-            CategoryModel(name: '卫衣'),
           ]);
     }));
     _controller.sink.add(_factories);
