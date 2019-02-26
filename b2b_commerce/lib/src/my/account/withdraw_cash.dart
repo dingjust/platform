2dd4f63306c838b227a4e0c77997754646b6f63a
huangweijun <weijun.huang@dingjust.com>
2019-02-26 14:53:09 +0800

    我的账户

---
+++ b2b_commerce/lib/src/my/account/withdraw_cash.dart
@@ -0,0 +1,377 @@
+import 'package:flutter/material.dart';
+import 'package:widgets/widgets.dart';
+
+/// 提现页面
+class WithdrawCash extends StatefulWidget {
+  _WithdrawCashState createState() => _WithdrawCashState();
+}
+
+class _WithdrawCashState extends State<WithdrawCash> {
+  String cash = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            child: ListView(
+              children: <Widget>[
+                _buildTopBackgroud(context),
+                _buildOne(context),
+              ],
+            )
+        )
+    );
+  }
+
+  Widget _buildTopBackgroud(BuildContext context) {
+    return Container(
+      padding:EdgeInsets.fromLTRB(0,5,0,0),
+      child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: IconButton(
+                          icon:Icon(Icons.chevron_left),
+                          color: Colors.white,
+                          iconSize: 30,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ),
+                      Expanded(
+                          child:Center(
+                            child: Container(
+                              padding: EdgeInsets.only(right: 30),
+                              child: Text(
+                                '提现',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          )
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin:EdgeInsets.fromLTRB(0,50,0,0),
+                child: Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Container(
+                          child: Text(
+                            "￥",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white
+                            ),
+                          )
+                      ),
+                      Container(
+                          child: Text(
+                            "987652.00",
+                            style: TextStyle(
+                                fontSize: 44,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white
+                            ),
+                          )
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin:EdgeInsets.fromLTRB(0,0,0,50),
+                child: Text(
+                  '余额',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white
+                  ),
+                ),
+              )
+            ],
+          )
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [ Color.fromRGBO(255, 80, 1, 1),Color.fromRGBO(255, 140, 0, 1)]),
+      ),
+    );
+  }
+
+  Widget _buildOne(BuildContext context){
+    return Container(
+      child: Column(
+        children: <Widget>[
+          _buildWithdrawCash(context),
+          Divider(
+            height: 1,
+          ),
+          _buildPoundage(context),
+          Divider(
+            height: 1,
+          ),
+          Container(
+            color: Colors.white,
+            child: ListTile(
+              leading: Text(
+                '提现到银行卡',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          _buildBlankCrad(context),
+          _buildCommitButton(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWithdrawCash(BuildContext context){
+    return Container(
+        color: Colors.white,
+        child:GestureDetector(
+            child: Container(
+              child: ListTile(
+                leading: Text(
+                  '提现金额￥',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                trailing: Text(
+                  '输入金额',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey
+                  ),
+                ),
+              ),
+            ),
+            onTap: () {
+              _neverInputCash(context);
+            })
+    );
+  }
+
+  Widget _buildPoundage(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: ListTile(
+        leading: Text(
+          '手续费(自动计算)',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        trailing: Text(
+          '￥0.00',
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBlankCrad(BuildContext context){
+    return Container(
+      padding: EdgeInsets.fromLTRB(20,0,20,20),
+      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(bottom: 15),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Image.network(
+                    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551089271298&di=e86274438d3c8ad92d09a82c412f7eb4&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f2b45541ad67000001a64b30c1bf.jpg',
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+                Expanded(
+                    child: Align(
+                        alignment:Alignment.centerLeft,
+                        child: Container(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment:MainAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: 150,
+                                child: Text(
+                                  '工商银行',
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                    '6212***************4243'
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                    )
+                ),
+                Icon(
+                    B2BIcons.del_blank_card
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      child:
+                      Text(
+                        '＋ 添加银行卡',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Color(0xFFFF9516),
+                        ),
+                      )
+                  ),
+                  Container(
+                    child: Text(
+                      '支持农行、工行、招行和建行',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black26, width: 0.5),
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+
+  //确认按钮
+  Widget _buildCommitButton(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 50,
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+            child: RaisedButton(
+              color: Color(0xFFFF9516),
+              child: Text(
+                '提交申请',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              onPressed: () {
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            width: 200,
+            child: Center(
+              child: Text(
+                '预计3天内到账',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  Future<void> _neverInputCash(BuildContext context) async {
+    TextEditingController inputNumber = TextEditingController();
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (context) {
+        return AlertDialog(
+          title: Text('请输入提现金额'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: inputNumber,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: '请输入提现金额',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('取消'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('确定'),
+              onPressed: () {
+                if (inputNumber.text != null) {
+                  print(inputNumber.text);
+                  setState(() {
+//                    processCount = inputNumber.text;
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+}
--- b2b_commerce/lib/src/my/my_account.dart
+++ b2b_commerce/lib/src/my/my_account.dart
@@ -1,72 +1,88 @@
+import 'package:b2b_commerce/src/my/account/withdraw_cash.dart';
 import 'package:flutter/material.dart';
+import 'package:widgets/widgets.dart';
 
 /// 我的账户
 class MyAccountPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              leading: GestureDetector(
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                ),
-                onTap: () => Navigator.pop(context),
-              ),
-              title: Text(
-                '我的账户',
-                style: TextStyle(
-                  color: Colors.white
-                ),
-              ),
-              centerTitle: true,
-              expandedHeight: 230,
-              pinned: true,
-              actions: <Widget>[
-                GestureDetector(
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                      child: Center(
-                        child: Text(
-                          '提现',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500
-                          ),
-                        ),
-                      ),
-                    ),
-                    onTap: () {
-                    }
-                )
+        body: Container(
+            child: ListView(
+              children: <Widget>[
+                _buildTopBackgroud(context),
+                _buildBill(context),
+                _buildBlankCard(context),
               ],
-              flexibleSpace: FlexibleSpaceBar(
-                background: Stack(
-//                  fit: StackFit.expand,
-                  children: <Widget>[
-                    _buildTopBackgroud(context),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+            )
+        )
     );
   }
 
   Widget _buildTopBackgroud(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 120),
+      padding:EdgeInsets.fromLTRB(0,5,0,0),
       child: Center(
           child: Column(
             children: <Widget>[
               Container(
+                child: Center(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          child: IconButton(
+                            icon:Icon(Icons.chevron_left),
+                            color: Colors.white,
+                            iconSize: 30,
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          )
+                      ),
+                      Expanded(
+                          child:Center(
+                            child: Container(
+                              padding: EdgeInsets.only(right: 30),
+                              child: Text(
+                                '我的账户',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          )
+                      ),
+                      GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                            child: Center(
+                              child: Text(
+                                '提现',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500
+                                ),
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => WithdrawCash(),
+                              ),
+                            );
+                          }
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin:EdgeInsets.fromLTRB(0,50,0,0),
                 child: Center(
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
@@ -96,8 +112,7 @@ class MyAccountPage extends StatelessWidget {
                 ),
               ),
               Container(
-                padding: EdgeInsets.all(3),
-                margin: EdgeInsets.only(left: 10),
+                margin:EdgeInsets.fromLTRB(0,0,0,50),
                 child: Text(
                   '余额',
                   style: TextStyle(
@@ -116,55 +131,179 @@ class MyAccountPage extends StatelessWidget {
     );
   }
 
-  Widget _buildMoney(BuildContext context){
+  Widget _buildBill(BuildContext context){
     return Container(
-      margin: EdgeInsets.only(top: 120),
-      child: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Center(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
+      color: Color.fromRGBO(245, 245, 245, 1),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Center(
+              child: Container(
+                  margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                  child: Text(
+                    "账单",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                    ),
+                  )
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20,10,20,10),
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15,0,15,0),
+                  margin: EdgeInsets.only(bottom: 15),
+                  child:Row(
                     children: <Widget>[
-                      Container(
-                          child: Text(
-                            "￥",
+                      Expanded(
+                          child:Text(
+                            '本月收支',
                             style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white
+                                fontSize: 20
                             ),
                           )
                       ),
+                      Icon(
+                        Icons.chevron_right,
+                        color: Colors.grey,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                ),
+                IncomeComparison(
+                  height: 120,
+                  income: 123456.00,
+                  expenditure: 789456.00,
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBlankCard(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(245, 245, 245, 1),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Center(
+              child: Container(
+                  margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                  child: Text(
+                    "银行卡",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18
+                    ),
+                  )
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20,0,20,20),
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: <Widget>[
                       Container(
-                          child: Text(
-                            "987652.00",
-                            style: TextStyle(
-                                fontSize: 44,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white
-                            ),
+                        child: Image.network(
+                          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551089271298&di=e86274438d3c8ad92d09a82c412f7eb4&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f2b45541ad67000001a64b30c1bf.jpg',
+                          width: 80,
+                          height: 80,
+                        ),
+                      ),
+                      Expanded(
+                          child: Align(
+                              alignment:Alignment.centerLeft,
+                              child: Container(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment:MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Container(
+                                      width: 150,
+                                      child: Text(
+                                        '工商银行',
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Text(
+                                          '6212***************4243'
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
                           )
+                      ),
+                      IconButton(
+                        icon: Icon(
+                            B2BIcons.del_blank_card
+                        ),
+                        tooltip: '删除银行卡',
                       )
                     ],
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(3),
-                margin: EdgeInsets.only(left: 10),
-                child: Text(
-                  '余额',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white
+                Container(
+//                    margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                            child:
+                            Text(
+                              '＋ 添加银行卡',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Color(0xFFFF9516),
+                              ),
+                            )
+                        ),
+                        Container(
+                          child: Text(
+                            '支持农行、工行、招行和建行',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black26, width: 0.5),
+                    borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
           )
+        ],
       ),
     );
   }
