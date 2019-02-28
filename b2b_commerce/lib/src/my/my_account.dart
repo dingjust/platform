8a411f52938b4fe4f2b5e171fe51d75c1de64088
huangweijun <weijun.huang@dingjust.com>
2019-02-28 14:07:53 +0800

    优化

--- b2b_commerce/lib/src/my/my_account.dart
+++ b2b_commerce/lib/src/my/my_account.dart
@@ -182,105 +182,92 @@ class MyAccountPage extends StatelessWidget {
     );
   }
 
-  Widget _buildBlankCard(BuildContext context) {
+  Widget _buildBlankCard(BuildContext context){
     return Container(
-      color: Color.fromRGBO(245, 245, 245, 1),
+      padding: EdgeInsets.fromLTRB(20,0,20,20),
+      margin: EdgeInsets.fromLTRB(10,20, 10, 10),
       child: Column(
         children: <Widget>[
           Container(
-            child: Center(
-              child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
-                  child: Text(
-                    "银行卡",
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
-                  )),
+            margin: EdgeInsets.only(bottom: 15),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Image.network(
+                    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551089271298&di=e86274438d3c8ad92d09a82c412f7eb4&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f2b45541ad67000001a64b30c1bf.jpg',
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+                Expanded(
+                    child: Align(
+                        alignment:Alignment.centerLeft,
+                        child: Container(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment:MainAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: 150,
+                                child: Text(
+                                  '工商银行',
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                    '6212***************4243'
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                    )
+                ),
+                Icon(
+                    B2BIcons.del_blank_card
+                )
+              ],
             ),
           ),
           Container(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-            margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom: 15),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Image.network(
-                          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551089271298&di=e86274438d3c8ad92d09a82c412f7eb4&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f2b45541ad67000001a64b30c1bf.jpg',
-                          width: 80,
-                          height: 80,
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      child:
+                      Text(
+                        '＋ 添加银行卡',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Color(0xFFFF9516),
                         ),
-                      ),
-                      Expanded(
-                          child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Container(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: <Widget>[
-                                    Container(
-                                      width: 150,
-                                      child: Text(
-                                        '工商银行',
-                                      ),
-                                    ),
-                                    Container(
-                                      child: Text('6212***************4243'),
-                                    )
-                                  ],
-                                ),
-                              ))),
-                      IconButton(
-                        icon: Icon(B2BIcons.del_blank_card),
-                        tooltip: '删除银行卡',
                       )
-                    ],
                   ),
-                ),
-                Container(
-//                    margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Center(
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                            child: Text(
-                          '＋ 添加银行卡',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                            color: Color(0xFFFF9516),
-                          ),
-                        )),
-                        Container(
-                          child: Text(
-                            '支持农行、工行、招行和建行',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                            ),
-                          ),
-                        )
-                      ],
+                  Container(
+                    child: Text(
+                      '支持农行、工行、招行和建行',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black26, width: 0.5),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
             decoration: BoxDecoration(
               color: Colors.white,
+              border: Border.all(color: Colors.black26, width: 0.5),
               borderRadius: BorderRadius.circular(5),
             ),
-          )
+          ),
         ],
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
       ),
     );
   }
