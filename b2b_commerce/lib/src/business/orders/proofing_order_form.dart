87d391765f9e73fd82f976490941903c9aa4697f
cjf <www.cjf.com>
2019-03-06 18:38:28 +0800

    需求订单筛选条件

---
+++ b2b_commerce/lib/src/business/orders/proofing_order_form.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProofingOrderFormPage extends StatefulWidget{
+
+  ProofingOrderFormPageState createState() => ProofingOrderFormPageState();
+}
+
+class ProofingOrderFormPageState extends State<ProofingOrderFormPage>{
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
