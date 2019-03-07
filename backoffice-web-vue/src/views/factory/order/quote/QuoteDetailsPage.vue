c2ad27dab65a5c3a569b44aca85d93ae377ee90a
zhanhongbo1112 <eric.zhan@163.com>
2019-03-07 20:25:09 +0800

    报价单

--- backoffice-web-vue/src/views/factory/order/quote/QuoteDetailsPage.vue
+++ backoffice-web-vue/src/views/factory/order/quote/QuoteDetailsPage.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="animated fadeIn">
-    <div class="pt-2"></div>
+    <!--<div class="pt-2"></div>
     <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>基本信息</span>
       </div>
       <quote-base-form :slot-data="slotData" :read-only="true"></quote-base-form>
+    </el-card>
+    <div class="pt-2"></div>-->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>报价明细</span>
+      </div>
+      <quote-request-form :slot-data="slotData" :read-only="true"></quote-request-form>
+    </el-card>
+    <div class="pt-2"></div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>附件</span>
+      </div>
+      <quote-attachments-form :slot-data="slotData" :read-only="true"></quote-attachments-form>
     </el-card>
     <div class="pt-2"></div>
   </div>
@@ -13,11 +27,13 @@
 
 <script>
   import QuoteBaseForm from './QuoteBaseForm';
+  import QuoteRequestForm from './QuoteRequestForm';
+  import QuoteAttachmentsForm from './QuoteAttachmentsForm';
 
   export default {
     name: 'QuoteDetailsPage',
     props: ['slotData', 'readOnly'],
-    components: {QuoteBaseForm},
+    components: {QuoteBaseForm, QuoteRequestForm, QuoteAttachmentsForm},
     methods: {},
     computed: {
       isNewlyCreated: function () {
