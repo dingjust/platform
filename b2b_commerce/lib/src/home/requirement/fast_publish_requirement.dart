754e0e9d4f9c81ad28e5b61cbc0b2e6c7c29aa6e
Konoha-orz <517136675@qq.com>
2019-03-13 14:50:48 +0800

    首页改版

--- b2b_commerce/lib/src/home/requirement/fast_publish_requirement.dart
+++ b2b_commerce/lib/src/home/requirement/fast_publish_requirement.dart
@@ -10,7 +10,8 @@ class FastPublishRequirement extends StatefulWidget {
 }
 
 class _FastPublishRequirementState extends State<FastPublishRequirement> {
-  RequirementOrderModel model = RequirementOrderModel(details: RequirementInfoModel());
+  RequirementOrderModel model =
+      RequirementOrderModel(details: RequirementInfoModel());
   GlobalKey _formKey = new GlobalKey<FormState>();
   TextEditingController _requirementNumController = TextEditingController();
 
@@ -25,7 +26,9 @@ class _FastPublishRequirementState extends State<FastPublishRequirement> {
 
   @override
   void initState() {
-    ProductRepositoryImpl().cascadedCategories().then((categorys)=>_categorys = categorys);
+    ProductRepositoryImpl()
+        .cascadedCategories()
+        .then((categorys) => _categorys = categorys);
 
     // TODO: implement initState
     super.initState();
@@ -39,7 +42,7 @@ class _FastPublishRequirementState extends State<FastPublishRequirement> {
       key: _formKey,
       child: Container(
         margin: EdgeInsets.only(top: 10),
-        padding: EdgeInsets.fromLTRB(8, 20, 8, 10),
+        padding: EdgeInsets.fromLTRB(8, 15, 8, 10),
         color: Colors.white,
         child: Column(
           children: <Widget>[
@@ -57,7 +60,7 @@ class _FastPublishRequirementState extends State<FastPublishRequirement> {
                     '   急速发布需求   ',
                     style: TextStyle(
                         color: Colors.red,
-                        fontSize: 15,
+                        fontSize: 18,
                         fontWeight: FontWeight.bold),
                   ),
                   Icon(
@@ -68,147 +71,173 @@ class _FastPublishRequirementState extends State<FastPublishRequirement> {
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                _showCategorySelect();
-              },
-              child: Container(
-                margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(5)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      category,
-                      style: TextStyle(
-                          color: Color.fromRGBO(150, 150, 150, 1),
-                          fontSize: 15),
-                    ),
-                    Icon(
-                      Icons.keyboard_arrow_down,
-                      color: Color.fromRGBO(200, 200, 200, 1),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                // _showRequirementNum(context, _focusNode);
-              },
-              child: Container(
-                margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(248, 248, 248, 1),
-                    borderRadius: BorderRadius.circular(5)),
-                child:
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    //   children: <Widget>[
-                    //     Text(
-                    //       requirementNum == null ? '填写需求数量' : '${requirementNum}',
-                    //       style: TextStyle(
-                    //           color: Color.fromRGBO(150, 150, 150, 1),
-                    //           fontSize: 15),
-                    //     )
-                    //   ],
-                    // )
-                    TextFormField(
-                        autofocus: false,
-                        controller: _requirementNumController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            hintStyle:
-                                TextStyle(color: Colors.grey, fontSize: 15),
-                            contentPadding: EdgeInsets.symmetric(vertical: 0),
-                            hintText: '填写需求数量',
-                            border: InputBorder.none),
-                        // 校验数量
-                        validator: (v) {
-                          return int.parse(v) > 0 ? null : '数量大于0';
-                        }),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _selectDate(context);
-              },
-              child: Container(
-                margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(5)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    expectedDeliveryDate != null
-                        ? Text(
-                            '${DateFormatUtil.format(expectedDeliveryDate)}',
-                            style: TextStyle(color: Colors.black, fontSize: 14),
-                          )
-                        : Text(
-                            '预计交货时间',
-                            style: TextStyle(color: Colors.grey, fontSize: 15),
-                          ),
-                    Icon(
-                      Icons.keyboard_arrow_down,
-                      color: Color.fromRGBO(200, 200, 200, 1),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    child: FlatButton(
-                        onPressed: () async{
-                          model.details.category = _categorySelected == [] ? null : _categorySelected[0];
-                          model.details.expectedMachiningQuantity = requirementNum;
-                          model.details.expectedDeliveryDate = expectedDeliveryDate;
-
-                          String code = await RequirementOrderRepository().publishNewRequirement(model);
-                          //清空数据
-                          expectedDeliveryDate = null;
-                          _requirementNumController.clear();
-
-                          //跳转
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    PublishRequirementSuccessDialog(
-                                      model: model,
-                                    ),
-                                fullscreenDialog: true,
-                              ));
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        color: Color.fromRGBO(255, 149, 22, 1),
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                        child: Text(
-                          '直接发布',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        )),
+            Container(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '点击',
+                    style: TextStyle(
+                        color: Color.fromRGBO(255, 214, 12, 1), fontSize: 18),
                   ),
-                )
-              ],
-            )
+                  Text(
+                    '   填写三步，剩下的工作交给蕉衣   ',
+                    style: TextStyle(
+                      color: Color.fromRGBO(180, 180, 180, 1),
+                      fontSize: 15,
+                    ),
+                  ),
+                  Icon(
+                    B2BIcons.arrow_right,
+                    color: Color.fromRGBO(255, 214, 12, 1),
+                    size: 12,
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+  // GestureDetector(
+  //             onTap: () {
+  //               _showCategorySelect();
+  //             },
+  //             child: Container(
+  //               margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
+  //               padding: EdgeInsets.all(10),
+  //               decoration: BoxDecoration(
+  //                   color: Colors.grey[100],
+  //                   borderRadius: BorderRadius.circular(5)),
+  //               child: Row(
+  //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                 children: <Widget>[
+  //                   Text(
+  //                     category,
+  //                     style: TextStyle(
+  //                         color: Color.fromRGBO(150, 150, 150, 1),
+  //                         fontSize: 15),
+  //                   ),
+  //                   Icon(
+  //                     Icons.keyboard_arrow_down,
+  //                     color: Color.fromRGBO(200, 200, 200, 1),
+  //                   )
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //           GestureDetector(
+  //             onTap: () {
+  //               // _showRequirementNum(context, _focusNode);
+  //             },
+  //             child: Container(
+  //               margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
+  //               padding: EdgeInsets.all(10),
+  //               decoration: BoxDecoration(
+  //                   color: Color.fromRGBO(248, 248, 248, 1),
+  //                   borderRadius: BorderRadius.circular(5)),
+  //               child:
+  //                   // Row(
+  //                   //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                   //   children: <Widget>[
+  //                   //     Text(
+  //                   //       requirementNum == null ? '填写需求数量' : '${requirementNum}',
+  //                   //       style: TextStyle(
+  //                   //           color: Color.fromRGBO(150, 150, 150, 1),
+  //                   //           fontSize: 15),
+  //                   //     )
+  //                   //   ],
+  //                   // )
+  //                   TextFormField(
+  //                       autofocus: false,
+  //                       controller: _requirementNumController,
+  //                       keyboardType: TextInputType.number,
+  //                       decoration: InputDecoration(
+  //                           hintStyle:
+  //                               TextStyle(color: Colors.grey, fontSize: 15),
+  //                           contentPadding: EdgeInsets.symmetric(vertical: 0),
+  //                           hintText: '填写需求数量',
+  //                           border: InputBorder.none),
+  //                       // 校验数量
+  //                       validator: (v) {
+  //                         return int.parse(v) > 0 ? null : '数量大于0';
+  //                       }),
+  //             ),
+  //           ),
+  //           GestureDetector(
+  //             onTap: () {
+  //               _selectDate(context);
+  //             },
+  //             child: Container(
+  //               margin: EdgeInsets.fromLTRB(0, 10, 0, 5),
+  //               padding: EdgeInsets.all(10),
+  //               decoration: BoxDecoration(
+  //                   color: Colors.grey[100],
+  //                   borderRadius: BorderRadius.circular(5)),
+  //               child: Row(
+  //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                 children: <Widget>[
+  //                   expectedDeliveryDate != null
+  //                       ? Text(
+  //                           '${DateFormatUtil.format(expectedDeliveryDate)}',
+  //                           style: TextStyle(color: Colors.black, fontSize: 14),
+  //                         )
+  //                       : Text(
+  //                           '预计交货时间',
+  //                           style: TextStyle(color: Colors.grey, fontSize: 15),
+  //                         ),
+  //                   Icon(
+  //                     Icons.keyboard_arrow_down,
+  //                     color: Color.fromRGBO(200, 200, 200, 1),
+  //                   )
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //           Row(
+  //             children: <Widget>[
+  //               Expanded(
+  //                 flex: 1,
+  //                 child: Container(
+  //                   margin: EdgeInsets.symmetric(vertical: 10),
+  //                   child: FlatButton(
+  //                       onPressed: () async{
+  //                         model.details.category = _categorySelected == [] ? null : _categorySelected[0];
+  //                         model.details.expectedMachiningQuantity = requirementNum;
+  //                         model.details.expectedDeliveryDate = expectedDeliveryDate;
+
+  //                         String code = await RequirementOrderRepository().publishNewRequirement(model);
+  //                         //清空数据
+  //                         expectedDeliveryDate = null;
+  //                         _requirementNumController.clear();
+
+  //                         //跳转
+  //                         Navigator.push(
+  //                             context,
+  //                             MaterialPageRoute(
+  //                               builder: (BuildContext context) =>
+  //                                   PublishRequirementSuccessDialog(
+  //                                     model: model,
+  //                                   ),
+  //                               fullscreenDialog: true,
+  //                             ));
+  //                       },
+  //                       shape: RoundedRectangleBorder(
+  //                           borderRadius: BorderRadius.circular(20)),
+  //                       color: Color.fromRGBO(255, 149, 22, 1),
+  //                       padding:
+  //                           EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+  //                       child: Text(
+  //                         '直接发布',
+  //                         style: TextStyle(color: Colors.white, fontSize: 16),
+  //                       )),
+  //                 ),
+  //               )
+  //             ],
+  //           )
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
