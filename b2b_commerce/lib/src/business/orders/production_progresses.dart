3541201b7b79a78a21290d48675350615476e965
huangweijun <weijun.huang@dingjust.com>
2019-03-26 19:18:56 +0800

    优化

--- b2b_commerce/lib/src/business/orders/production_progresses.dart
+++ b2b_commerce/lib/src/business/orders/production_progresses.dart
@@ -194,7 +194,7 @@ class _ProductionProgressesPageState extends State<ProductionProgressesPage> {
                   Navigator.of(context).push(MaterialPageRoute(
                       builder: (context) => PicturePickPreviewWidget(
                         medias: progress.medias,
-                        isUpload: sequence > _index || phase == currentPhase? true : false,
+                        isUpload: userType != null && userType == 'factory' && (sequence >= _index || phase == currentPhase) ? true : false,
                       ))
                   ).then((value){
                     if(value != null){
