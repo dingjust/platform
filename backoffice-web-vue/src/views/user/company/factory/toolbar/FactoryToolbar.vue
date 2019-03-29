234469039bb90ab1549e8d013b7a6799bebaaca7
lihongjiang <lihongjiang@dingjust.com>
2019-03-29 11:21:06 +0800

    品牌方仪表盘和品台工厂高级查询

--- backoffice-web-vue/src/views/user/company/factory/toolbar/FactoryToolbar.vue
+++ backoffice-web-vue/src/views/user/company/factory/toolbar/FactoryToolbar.vue
@@ -5,13 +5,13 @@
     </el-form-item>
     <el-button-group>
       <el-button type="primary" icon="el-icon-search" @click="onSearch"></el-button>
-      <el-button v-if="!hideCreationButton"  type="primary" icon="el-icon-plus" @click="onNew">新增</el-button>
+      <el-button v-if="isTenant()"  type="primary" icon="el-icon-plus" @click="onNew">新增</el-button>
     </el-button-group>
-    <el-popover placement="bottom" width="800" trigger="click">
+    <el-popover placement="bottom" width="800" trigger="click" v-model="visible" >
       <el-row :gutter="10">
         <el-col :span="8">
           <el-form-item label="标签" prop="labels">
-            <el-select class="w-100" v-model="queryFormData.labels" value-key="id" multiple>
+            <el-select class="w-100" v-model="queryFormData.labels" value-key="id" multiple filterable>
               <el-option v-for="item in labels"
                          :label="item.name"
                          :key="item.id"
@@ -22,7 +22,7 @@
         </el-col>
         <el-col :span="8">
           <el-form-item label="公司规模" prop="populationScales">
-            <el-select v-model="queryFormData.populationScales" class="w-100" multiple>
+            <el-select v-model="queryFormData.populationScales" class="w-100" multiple filterable>
               <el-option v-for="item in populationScales"
                          :key="item.code"
                          :label="item.name"
@@ -33,13 +33,56 @@
         </el-col>
         <el-col :span="8">
           <el-form-item label="生产地区" prop="regions">
-            <el-select v-model="queryFormData.regions" class="w-100" multiple>
+            <el-select v-model="queryFormData.regions" class="w-100" multiple filterable>
               <el-option v-for="item in regions"
                          :key="item.isocode"
                          :label="item.name"
                          :value="item.isocode">
               </el-option>
             </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="10">
+        <el-col :span="8">
+          <el-form-item label="分类" prop="adeptAtCategories">
+            <el-select class="w-100"
+                       placeholder="请选择"
+                       v-model="queryFormData.adeptAtCategories"
+                       value-key="code" multiple filterable>
+              <el-option-group
+                v-for="group in adeptAtCategories"
+                :key="group.code"
+                :label="group.name">
+                <el-option
+                  v-for="item in group.children"
+                  :key="item.code"
+                  :label="item.name"
+                  :value="item">
+                </el-option>
+              </el-option-group>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="大类" prop="categories">
+            <el-select class="w-100" v-model="queryFormData.categories" value-key="id" multiple filterable>
+              <el-option v-for="item in categories"
+                         :label="item.name"
+                         :key="item.name"
+                         :value="item">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="10">
+        <el-col :span="6">
+          <el-form-item label="评级" prop="starLevel">
+            <el-rate
+              v-model="queryFormData.starLevel"
+              :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+            </el-rate>
           </el-form-item>
         </el-col>
       </el-row>
@@ -67,13 +110,19 @@
     computed: {},
     methods: {
       ...mapMutations({
-        setKeyword: 'keyword'
+        setKeyword: 'keyword',
+        setQueryFormData:'queryFormData',
       }),
       onSearch() {
         this.setKeyword(this.keyword);
         this.$emit('onSearch', 0);
       },
       onAdvancedSearch() {
+        if(this.queryFormData.starLevel == 0){
+          this.queryFormData.starLevel = null;
+        }
+        this.setQueryFormData(this.queryFormData);
+        this.visible = false;
         this.$emit('onAdvancedSearch', 0);
       },
       onNew() {
@@ -83,7 +132,7 @@
         this.$emit('onNew', formData);
       },
       async getLabels() {
-        const url = this.apis().getAllLabels();
+        const url = this.apis().getGroupAllLabels('FACTORY');
         const result = await this.$http.get(url);
         if (result["errors"]) {
           this.$message.error(result["errors"][0].message);
@@ -102,12 +151,35 @@
 
         this.regions = result;
       },
+      async getCategories() {
+        const url = this.apis().getMajorCategories();
+        const result = await this.$http.get(url);
+        if (result["errors"]) {
+          this.$message.error(result["errors"][0].message);
+          return;
+        }
+
+        this.categories = result;
+      },
+      async getMinorCategories() {
+        const url = this.apis().getMinorCategories();
+        const result = await this.$http.get(url);
+        if (result["errors"]) {
+          this.$message.error(result["errors"][0].message);
+          return;
+        }
+
+        this.adeptAtCategories = result;
+      },
     },
     data() {
       return {
+        visible:false,
         keyword: '',
         labels: [],
         regions: [],
+        categories:[],
+        adeptAtCategories:[],
         formData: this.$store.state.FactoriesModule.formData,
         queryFormData: this.$store.state.FactoriesModule.queryFormData,
         populationScales: this.$store.state.EnumsModule.populationScales,
@@ -116,6 +188,8 @@
     created() {
       this.getLabels();
       this.getRegions();
+      this.getCategories();
+      this.getMinorCategories();
     }
   }
 </script>
