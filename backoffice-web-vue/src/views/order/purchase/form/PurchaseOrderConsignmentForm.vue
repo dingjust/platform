5f4c9738fccbbc53ee4d92ca733407b9abedb5e6
zhanhongbo1112 <eric.zhan@163.com>
2019-04-03 15:08:33 +0800

    确认发货

---
+++ backoffice-web-vue/src/views/order/purchase/form/PurchaseOrderConsignmentForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="animated fadeIn">
+    <consignment-form :slot-data="slotData.consignment" :read-only="true"/>
+  </div>
+</template>
+
+<script>
+  import ConsignmentForm from "@/views/shared/order/consignment/consignment-form";
+
+  export default {
+    name: 'PurchaseOrderConsignmentForm',
+    components: {ConsignmentForm},
+    props: ['slotData', 'readOnly'],
+    mixins: [],
+    computed: {},
+    methods: {},
+    data() {
+      return {}
+    },
+    created() {
+    }
+  }
+</script>
