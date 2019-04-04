e9b9c9d813352f3a4d0c4b452fe17fbdcb919d1d
lihongjiang <lihongjiang@dingjust.com>
2019-04-04 12:00:15 +0800

    PC推荐需求订单给工厂必须是报价中状态

--- backoffice-web-vue/src/views/order/requirement/toolbar/RequirementOrderOperateToolbar.vue
+++ backoffice-web-vue/src/views/order/requirement/toolbar/RequirementOrderOperateToolbar.vue
@@ -3,7 +3,7 @@
     <el-button-group>
       <el-button  v-if="isTenant()" size="mini" type="primary"  @click="onReview">审核</el-button>
       <el-button v-if="readOnly&&!isFactory()" size="mini" type="primary" @click="onUpdate()">修改</el-button>
-      <el-button  v-if="!isFactory()" size="mini" type="primary"  @click="onRecommended()">推荐</el-button>
+      <el-button  v-if="!isFactory()&&canRecommended" size="mini" type="primary"  @click="onRecommended()">推荐</el-button>
     </el-button-group>
   </div>
 </template>
@@ -11,7 +11,7 @@
 <script>
   export default {
     name: 'RequirementOperateFormToolbar',
-    props: ['readOnly'],
+    props: ['readOnly','canRecommended'],
     mixins: [],
     components: {},
     computed: {},
