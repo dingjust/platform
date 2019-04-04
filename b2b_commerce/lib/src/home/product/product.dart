d37a534392e982a67c57984ac14398211b2803f8
huangweijun <weijun.huang@dingjust.com>
2019-04-04 12:04:16 +0800

    优化

--- b2b_commerce/lib/src/home/product/product.dart
+++ b2b_commerce/lib/src/home/product/product.dart
@@ -59,43 +59,51 @@ class RecommendProductItem extends StatelessWidget {
                     borderRadius: BorderRadius.circular(5)),
                 padding: EdgeInsets.all(5),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      '${model.name}',
-                      overflow: TextOverflow.ellipsis,
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        '${model.name}',
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                    RichText(
-                      text: TextSpan(
-                          text: '￥',
-                          style: TextStyle(color: Colors.red, fontSize: 14),
-                          children: <TextSpan>[
-                            TextSpan(
-                                text:
-                                    '${model.minPrice ?? 0}—${model.maxPrice ?? 0}',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold)),
-                          ]),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      child: RichText(
+                        text: TextSpan(
+                            text: '￥',
+                            style: TextStyle(color: Colors.red, fontSize: 14),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text:
+                                  '${model.minPrice ?? 0}—${model.maxPrice ?? 0}',
+                                  style: TextStyle(
+                                      fontSize: 16, fontWeight: FontWeight.bold)),
+                            ]),
+                      ),
                     ),
                     showAddress
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                '${model.belongTo.contactAddress.region.name}${model.belongTo.contactAddress.city.name}',
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: Color.fromRGBO(149, 149, 149, 1)),
-                              ),
-                              Text(
-                                '',
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: Color.fromRGBO(149, 149, 149, 1)),
-                              )
-                            ],
+                        ? Container(
+                      padding: EdgeInsets.all(5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            '${model.belongTo.contactAddress.region.name}${model.belongTo.contactAddress.city.name}',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(149, 149, 149, 1)),
+                          ),
+                          Text(
+                            '',
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromRGBO(149, 149, 149, 1)),
                           )
+                        ],
+                      ),
+                    )
                         : Container()
                   ],
                 ),
