b9806cc6e401a5998cc8a619f85b96a2c97309c0
zhanhongbo1112 <eric.zhan@163.com>
2019-04-11 14:00:38 +0800

    137 localhost

--- c2b-web/src/services/login.js
+++ c2b-web/src/services/login.js
@@ -1,7 +1,7 @@
 import request from '../utils/request'
 export function getCode({ phone }) {
     const key = phone.replace(/\s+/g, "");
-    return request({ method: 'get', url: `/c2b/c2b/sms/asm/${key}` });
+    return request({ method: 'get', url: `/c2b/c2b/sms/captcha/asm/${key}` });
 }
 export function login({ code }) {
     return request({ method: 'post', url: `/authorizationserver/oauth/token?client_id=asm&client_secret=password&grant_type=authorization_code&code=${code}` });
