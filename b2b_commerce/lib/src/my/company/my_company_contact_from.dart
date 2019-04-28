e3271ada13a55be37a3ffdc1d6532a9056b59360
huangweijun <weijun.huang@dingjust.com>
2019-04-28 15:21:45 +0800

    优化

--- b2b_commerce/lib/src/my/company/my_company_contact_from.dart
+++ b2b_commerce/lib/src/my/company/my_company_contact_from.dart
@@ -1,8 +1,10 @@
 import 'package:b2b_commerce/src/my/address/contact_address_form.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:models/models.dart';
 import 'package:services/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:widgets/widgets.dart';
 
 class MyCompanyContactFromPage extends StatefulWidget{
@@ -197,24 +199,29 @@ class _MyCompanyContactFromPageState extends State<MyCompanyContactFromPage>{
           controller: phoneController,
         ),
       )
-        :ListTile(
-        leading: Container(
-          child: Text(
-            '联系电话',
-            style: TextStyle(
-              fontSize: 16,
+        :GestureDetector(
+        child: ListTile(
+          leading: Container(
+            child: Text(
+              '联系电话',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          trailing : Container(
+            child: Text(
+              '${widget.company.contactPhone==null?'':widget.company.contactPhone}',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey
+              ),
             ),
           ),
         ),
-        trailing : Container(
-          child: Text(
-            '${widget.company.contactPhone==null?'':widget.company.contactPhone}',
-            style: TextStyle(
-              fontSize: 16,
-                color: Colors.grey
-            ),
-          ),
-        ),
+        onTap: (){
+          _selectActionButton(widget.company.contactPhone);
+        },
       ),
     );
   }
@@ -279,24 +286,29 @@ class _MyCompanyContactFromPageState extends State<MyCompanyContactFromPage>{
           controller: telController,
         ),
       )
-          :ListTile(
-        leading: Container(
-          child: Text(
-            '座机号码',
-            style: TextStyle(
-              fontSize: 16,
+          :GestureDetector(
+        child: ListTile(
+          leading: Container(
+            child: Text(
+              '座机号码',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          trailing : Container(
+            child: Text(
+              '${widget.company.phone== null ? '':widget.company.phone}',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey
+              ),
             ),
           ),
         ),
-        trailing : Container(
-          child: Text(
-            '${widget.company.phone== null ? '':widget.company.phone}',
-            style: TextStyle(
-              fontSize: 16,
-                color: Colors.grey
-            ),
-          ),
-        ),
+        onTap: (){
+          _selectActionButton(widget.company.phone);
+        },
       ),
     );
   }
@@ -323,24 +335,29 @@ class _MyCompanyContactFromPageState extends State<MyCompanyContactFromPage>{
           controller: emailController,
         ),
       )
-          :ListTile(
-        leading: Container(
-          child: Text(
-            '邮箱地址',
-            style: TextStyle(
-              fontSize: 16,
+          :GestureDetector(
+        child: ListTile(
+          leading: Container(
+            child: Text(
+              '邮箱地址',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          trailing : Container(
+            child: Text(
+              '${widget.company.email==null?'':widget.company.email}',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey
+              ),
             ),
           ),
         ),
-        trailing : Container(
-          child: Text(
-            '${widget.company.email==null?'':widget.company.email}',
-            style: TextStyle(
-              fontSize: 16,
-                color: Colors.grey
-            ),
-          ),
-        ),
+        onTap: (){
+          copyToClipboard(widget.company.email,context);
+        },
       ),
     );
   }
@@ -367,72 +384,141 @@ class _MyCompanyContactFromPageState extends State<MyCompanyContactFromPage>{
           controller: QQController,
         ),
       )
-          :ListTile(
-        leading: Container(
-          child: Text(
-            'QQ号码',
-            style: TextStyle(
-              fontSize: 16,
-            ),
-          ),
-        ),
-        trailing : Container(
-          child: Text(
-            '${widget.company.qq==null?'':widget.company.qq}',
-            style: TextStyle(
+          :GestureDetector(
+        child: ListTile(
+          leading: Container(
+            child: Text(
+              'QQ号码',
+              style: TextStyle(
                 fontSize: 16,
-                color: Colors.grey
+              ),
+            ),
+          ),
+          trailing : Container(
+            child: Text(
+              '${widget.company.qq==null?'':widget.company.qq}',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey
+              ),
             ),
           ),
         ),
+        onTap: (){
+          copyToClipboard(widget.company.qq,context);
+        },
       ),
     );
   }
 
   Widget _buildWeChar(BuildContext context){
     return Container(
-      child: isEditing?
-      Container(
-        color: Colors.white,
-        margin: EdgeInsets.only(top: 10),
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: TextFieldComponent(
-          padding: EdgeInsets.symmetric(vertical: 5),
-          dividerPadding: EdgeInsets.only(),
-          focusNode: weCharFocusNode,
-          leadingText: Text('微信号码',
-              style: TextStyle(
-                fontSize: 16,
-              )),
-          hintText: '填写',
-          autofocus: false,
-          inputType: TextInputType.text,
-          textAlign: TextAlign.right,
-          controller: weCharController,
-        ),
-      )
-          :ListTile(
-        leading: Container(
-          child: Text(
-            '微信号码',
-            style: TextStyle(
-              fontSize: 16,
+      child: Container(
+        child: isEditing?
+        Container(
+          color: Colors.white,
+          margin: EdgeInsets.only(top: 10),
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: TextFieldComponent(
+            padding: EdgeInsets.symmetric(vertical: 5),
+            dividerPadding: EdgeInsets.only(),
+            focusNode: weCharFocusNode,
+            leadingText: Text('微信号码',
+                style: TextStyle(
+                  fontSize: 16,
+                )),
+            hintText: '填写',
+            autofocus: false,
+            inputType: TextInputType.text,
+            textAlign: TextAlign.right,
+            controller: weCharController,
+          ),
+        )
+            :GestureDetector(
+          child: ListTile(
+            leading: Container(
+              child: Text(
+                '微信号码',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            trailing : Container(
+              child: Text(
+                '${widget.company.wechat==null?'':widget.company.wechat}',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey
+                ),
+              ),
             ),
           ),
-        ),
-        trailing : Container(
-          child: Text(
-            '${widget.company.wechat==null?'':widget.company.wechat}',
-            style: TextStyle(
-              fontSize: 16,
-                color: Colors.grey
-            ),
-          ),
+          onTap: (){
+            copyToClipboard(widget.company.wechat,context);
+          },
         ),
       ),
     );
   }
 
+  //拨打电话或发短信
+  void _selectActionButton(String tel) async {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.phone),
+              title: const Text('拨打电话'),
+              onTap: () async {
+                var url = 'tel:' + tel;
+                await launch(url);
+              },
+            ),
+            tel.indexOf('-') > -1
+                ? Container()
+                : ListTile(
+              leading: const Icon(Icons.message),
+              title: const Text('发送短信'),
+              onTap: () async {
+                var url = 'sms:' + tel;
+                await launch(url);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
+  copyToClipboard(final String text, BuildContext context) {
+    if (text == null) return;
+    Clipboard.setData(ClipboardData(text: text));
+    _neverCopyContent(context);
+  }
+
+  Future<void> _neverCopyContent(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (context) {
+        return AlertDialog(
+          title: Text('消息'),
+          content: Text('复制成功'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('确定'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
 }
