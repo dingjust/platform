4f4d93ac88456a251619a980afe79fb369eb4644
Konoha-orz <517136675@qq.com>
2019-05-06 09:30:30 +0800

    配置修改

--- core/lib/src/config/configs.dart
+++ core/lib/src/config/configs.dart
@@ -2,13 +2,13 @@ class GlobalConfigs {
   static const PAGE_SIZE = 20;
   static const DEBUG = true;
   //测试
-  static const CONTEXT_PATH = 'http://sc.nbyjy.net';
-  static const MEDIA_CONTEXT_PATH = 'http://sc.img.nbyjy.net';
+  // static const CONTEXT_PATH = 'http://sc.nbyjy.net';
+  // static const MEDIA_CONTEXT_PATH = 'http://sc.nbyjy.net';
   // static const IMAGE_BASIC_URL = 'http://47.106.112.137:9001/djwebservices/v2';
   // static const CONTEXT_PATH = 'https://192.168.1.191:9002';
   // 生产
-  // static const CONTEXT_PATH = 'http://ht.nbyjy.net';
-  // static const MEDIA_CONTEXT_PATH = 'http://img.nbyjy.net';
+  static const CONTEXT_PATH = 'http://ht.nbyjy.net';
+  static const MEDIA_CONTEXT_PATH = 'http://img.nbyjy.net';
   // static const CONTEXT_PATH = 'http://platform.dingjust.cn';
   // static const IMAGE_BASIC_URL = 'http://120.78.211.86:9001/djwebservices/v2';
 
