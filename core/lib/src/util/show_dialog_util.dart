9371f49f5960d8079b4bd3791aa34199a2863c94
cjf <www.cjf.com>
2019-05-15 15:16:01 +0800

    showDialogUtil

--- core/lib/src/util/show_dialog_util.dart
+++ core/lib/src/util/show_dialog_util.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ShowDialogUtil{
+class ShowDialogUtil extends StatelessWidget{
 
   //操作提示弹框
   static Future<void> showAlertDialog(BuildContext context,String text,VoidCallback onPressed){
@@ -41,6 +41,43 @@ class ShowDialogUtil{
     );
   }
 
+  //操作提示弹框
+  static Future<void> showAlertDialog2(BuildContext context,String text,VoidCallback onCanel,VoidCallback onPressed){
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (context) {
+        return AlertDialog(
+          title: Text('提示',
+            style: TextStyle(
+              fontSize: 16,
+            ),),
+          content: Text(text),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                '取消',
+                style: TextStyle(
+                    color: Colors.grey
+                ),
+              ),
+              onPressed: onCanel,
+            ),
+            FlatButton(
+              child: Text(
+                '确定',
+                style: TextStyle(
+                    color: Colors. black
+                ),
+              ),
+              onPressed: onPressed,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   //提示弹框
   static Future<void> showSimapleDialog(BuildContext context,String message){
       return showDialog<void>(
@@ -53,5 +90,10 @@ class ShowDialogUtil{
           );
         },
       );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
   }
 }
