01689d778829a54c51b717c4b95e2a7723c375ac
Konoha-orz <517136675@qq.com>
2019-09-10 17:44:58 +0800

    sourceMap 关闭

--- backoffice-web-vue/build/webpack.prod.conf.js
+++ backoffice-web-vue/build/webpack.prod.conf.js
@@ -35,7 +35,7 @@ let webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: true
+      sourceMap: false
     }),
     // extract css into its own file
     new ExtractTextPlugin({
