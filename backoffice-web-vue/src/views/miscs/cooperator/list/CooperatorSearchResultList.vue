ef7ed7bf7721331bf9df4d02e744759921eb55b6
cjf <www.cjf.com>
2019-09-19 17:38:00 +0800

    合作商

--- backoffice-web-vue/src/views/miscs/cooperator/list/CooperatorSearchResultList.vue
+++ backoffice-web-vue/src/views/miscs/cooperator/list/CooperatorSearchResultList.vue
@@ -105,7 +105,13 @@
         this.$emit('onEdit', row);
       },
       onDelete(row) {
-        this.$emit('onDelete', row);
+        this.$confirm('此操作将永久删除该合作商, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$emit('onDelete', row);
+        })
       },
     },
     data() {
