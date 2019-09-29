d64b2923ed8a42a1e33f33d1d2e7ee10bfbbbc4a
Konoha-orz <517136675@qq.com>
2019-09-29 14:20:08 +0800

    上传组件

--- backoffice-web-vue/src/components/custom/ImagesUpload.vue
+++ backoffice-web-vue/src/components/custom/ImagesUpload.vue
@@ -2,18 +2,10 @@
   <div class="animated fadeIn">
     <el-row :gutter="10">
       <el-col :span="24">
-        <el-upload
-          name="file"
-          :action="mediaUploadUrl"
-          list-type="picture-card"
-          :data="uploadFormData"
-          :before-upload="onBeforeUpload"
-          :on-success="onSuccess"
-          :headers="headers"
-          :file-list="fileList"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove">
-          <i class="el-icon-plus"></i>
+        <el-upload name="file" :action="mediaUploadUrl" list-type="picture-card" :data="uploadFormData"
+          :before-upload="onBeforeUpload" :on-success="onSuccess" :headers="headers" :file-list="fileList"
+          :on-exceed="handleExceed" :on-preview="handlePreview" :limit="limit" :on-remove="handleRemove">
+          <i class="el-icon-plus" slot="default"></i>
         </el-upload>
         <el-dialog :visible.sync="dialogVisible" :modal="false">
           <img width="100%" :src="dialogImageUrl" alt="">
@@ -26,14 +18,22 @@
 <script>
   export default {
     name: 'ImagesUpload',
-    props: ['slotData'],
+    props: {
+      slotData: {
+        type: Array,
+        required: true
+      },
+      limit: {
+        type: Number,
+        default: 5
+      }
+    },
     methods: {
       onBeforeUpload(file) {
         if (file.size > 1024 * 1024 * 10) {
           this.$message.error('上传的文件不允许超过10M');
           return false;
         }
-
         return true;
       },
       onSuccess(response) {
@@ -42,49 +42,55 @@
       async handleRemove(file) {
         // console.log(JSON.stringify(file));
         // TODO: 自定义删除方法（删除图片之前，清理product的others属性
-        const url = this.apis().removeMedia(file.id);
-        const result = await this.$http.delete(url);
-        if (result['errors']) {
-          this.$message.error(result['errors'][0].message);
-          return;
-        }
+        // const url = this.apis().removeMedia(file.id);
+        // const result = await this.$http.delete(url);
+        // if (result['errors']) {
+        //   this.$message.error(result['errors'][0].message);
+        //   return;
+        // }
+        // const images = this.fileList || [];
+        // const index = images.indexOf(file);
+        // images.splice(index, 1);
+        // let newImages = [];
+        // this.slotData.forEach(image => {
+        //   if (image.id != file.id) {
+        //     newImages.push(image);
+        //   }
+        // });
+        // this.slotData = newImages;
+        // this.$message.success("删除成功");
+
         const images = this.fileList || [];
         const index = images.indexOf(file);
-        images.splice(index, 1);
-        let newImages = [];
-        this.slotData.forEach(image=>{
-          if(image.id != file.id){
-            newImages.push(image);
-          }
-        });
-        this.slotData = newImages;
-        this.$message.success("删除成功");
+        this.slotData.splice(index, 1);
       },
       handlePreview(file) {
         this.dialogImageUrl = file.url;
         this.dialogVisible = true;
-      }
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning('当前限制选择' + this.limit + ' 个图片');
+      },
     },
     computed: {
-      fileList:function(){
+      fileList: function () {
         let files = [];
-        if(this.slotData&&this.slotData.length >0){
-          this.slotData.forEach(image=>{
+        if (this.slotData && this.slotData.length > 0) {
+          this.slotData.forEach(image => {
             let file = {
-              id:"",
-              url:"",
-              artworkUrl:"",
+              id: "",
+              url: "",
+              artworkUrl: "",
             }
             file.id = image.id;
             file.artworkUrl = image.url;
             file.url = image.url;
             // image.url = '';
-            if(image.convertedMedias.length > 0){
-              image.convertedMedias.forEach(convertedMedia=>{
-                  if(convertedMedia.mediaFormat === 'DefaultProductPreview')
-                    file.url = convertedMedia.url;
-                }
-              )
+            if (image.convertedMedias.length > 0) {
+              image.convertedMedias.forEach(convertedMedia => {
+                if (convertedMedia.mediaFormat === 'DefaultProductPreview')
+                  file.url = convertedMedia.url;
+              })
             }
             files.push(file);
           })
@@ -110,4 +116,5 @@
       }
     },
   };
+
 </script>
