f14209c4dffdaeaca22bb0db494b54837050cf59
Konoha-orz <517136675@qq.com>
2020-01-15 17:01:08 +0800

    生产进度单据调整

--- backoffice-web-vue/config/index.js
+++ backoffice-web-vue/config/index.js
@@ -132,6 +132,58 @@ module.exports = {
     // },
     // proxyTable: {
     //   '/resource': {
+    //     target: 'https://ht.nbyjy.net',
+    //     "secure": false,
+    //   },
+    //   '/medias': {
+    //     target: 'https://120.78.211.86:9002',
+    //     "secure": false,
+    //     pathRewrite: {
+    //       '^/medias': 'djwebservices/medias'
+    //     }
+    //   },
+    //   '/logout': {
+    //     target: 'https://120.78.211.86:9002',
+    //     "secure": false,
+    //     pathRewrite: {
+    //       '^/logout': 'djwebservices/logout'
+    //     }
+    //   },
+    //   '/authorizationserver': {
+    //     target: 'https://120.78.211.86:9002',
+    //     changeOrigin: true,
+    //     "secure": false,
+    //     pathRewrite: {
+    //       '^/authorizationserver': 'authorizationserver'
+    //     }
+    //   },
+    //   '/b2b': {
+    //     target: 'https://120.78.211.86:9002',
+    //     changeOrigin: true,
+    //     "secure": false,
+    //     pathRewrite: {
+    //       '^/b2b': 'djwebservices/v2/b2b-apparel-zh'
+    //     }
+    //   },
+    //   '/c2b': {
+    //     target: 'https://120.78.211.86:9002',
+    //     changeOrigin: true,
+    //     "secure": false,
+    //     pathRewrite: {
+    //       '^/c2b': 'djwebservices/v2/c2b-apparel-zh'
+    //     }
+    //   },
+    //   '/amap': {
+    //     target: 'https://restapi.amap.com',
+    //     changeOrigin: true,
+    //     "secure": false,
+    //     pathRewrite: {
+    //       '^/amap': ''
+    //     }
+    //   },
+    // },
+    // proxyTable: {
+    //   '/resource': {
     //     target: 'http://192.168.31.179',
     //     "secure": false,
     //   },
