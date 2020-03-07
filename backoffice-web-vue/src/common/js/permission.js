f0c4a73f93e642ef8593008fb6cf6124c7bd885f
lgy <13660339641@163.com>
2020-03-07 13:44:27 +0800

    权限列表修改

--- backoffice-web-vue/src/common/js/permission.js
+++ backoffice-web-vue/src/common/js/permission.js
@@ -1,5 +1,5 @@
 // 需求订单
-const requirementOrderPage = ['REQUIREMENT_ORDER_PAGE']
+const requirementOrder = ['REQUIREMENT_ORDER']
 const requirementOrderPublish = ['REQUIREMENT_ORDER_PUBLISH']
 const requirementOrderDetail = ['REQUIREMENT_ORDER_DETAIL']
 const requirementOrderRecommended = ['REQUIREMENT_ORDER_RECOMMENDED']
@@ -8,66 +8,68 @@ const requirementOrderChooseFactory = ['REQUIREMENT_ORDER_CHOOSEFACTORY']
 const requirementOrderModify = ['REQUIREMENT_ORDER_MODIFY']
 
 // 报价订单
-const qutoOrderPage = ['QUTO_ORDER_PAGE']
-const qutoOrderDetail = ['QUTO_ORDER_DETAIL']
+const quoteOrder = ['QUOTE_ORDER']
+const quoteOrderDetail = ['QUOTE_ORDER_DETAIL']
 const proofingOrderCreate = ['PROOFING_ORDER_CREATE']
 const purchaseOrderCreate = ['PURCHASE_ORDER_CREATE']
-const qutoOrderClose = ['QUTO_ORDER_CLOSE']
+const quoteOrderClose = ['QUOTE_ORDER_CLOSE']
 
 // 打样订单
-const proofingOrderPage = ['PROOFING_ORDER_PAGE']
+const proofingOrder = ['PROOFING_ORDER']
 const proofingOrderView = ['PROOFING_ORDER_VIEW']
-const proofingOrderDeliveryAddress = ['PROOFING_ORDER_DELIVERY_ADDRESS']
+const proofingOrderDeliveryAddress = ['PROOFING_ORDER_DELIVERYADDRESS']
 const proofingOrderClose = ['PROOFING_ORDER_CLOSE']
 
 // 生产订单
-const purchaseOrderPage = ['PURCHASE_ORDER_PAGE']
+const purchaseOrder = ['PURCHASE_ORDER']
 const purchaseOrderDetail = ['PURCHASE_ORDER_DETAIL']
 const purchaseOrderOperate = ['PURCHASE_ORDER_OPERATE']
 const purchaseOrderPayplanOp = ['PURCHASE_ORDER_PAYPLAN_OP']
 const purchaseOrderClose = ['PURCHASR_ORDER_CLOSE']
-const purchaseOrderAccept = ['PURCHASR_ORDER_ACCEPT']
-const purchaseOrderReject = ['PURCHASR_ORDER_REJECT']
 const purchaseOrderOfflineCreate = ['PURCHASR_ORDER_OFFLINE_CREATE']
 const purchaseOrderUniqueCodeImport = ['PURCHASR_ORDER_UNIQUECODE_IMPORT']
+const purchaseOrderConfirmDelivering = ['PURCHASE_ORDER_CONFIRM_DELIVERING']
+const purchaseOrderConfirmReceived = ['PURCHASE_ORDER_CONFIRM_RECEIVED']
+const purchaseOrderConfirm = ['PURCHASE_ORDER_CONFIRM']
+const purchaseOrderReconciliation = ['PURCHASE_ORDER_RECONCILIATION']
 
 // 合同管理
-const agreementPage = ['AGREEMENT_PAGE']
+const agreement = ['AGREEMENT']
 const agreementSign = ['AGREEMENT_SIGN']
 
 // 合同模板
-const agreementTmplPage = ['AGREEMENT_TMPL_PAGE']
+const agreementTmpl = ['AGREEMENT_TMPL']
 const agreementTmplCreate = ['AGREEMENT_TMPL_CREATE']
 const agreementTmplView = ['AGREEMENT_TMPL_VIEW']
 const agreementTmplDelete = ['AGREEMENT_TMPL_DELETE']
 
 // 签章管理
-const agreementSealPage = ['AGREEMENT_SEAL_PAGE']
+const agreementSeal = ['AGREEMENT_SEAL']
 const agreementSealCreate = ['AGREEMENT_SEAL_CREATE']
 const agreementSealRemove = ['AGREEMENT_SEAL_REMOVE']
 
 // 产品管理
-const productPage = ['PRODUCT_PAGE']
+const product = ['PRODUCT']
 const productCreate = ['PRODUCT_CREATE']
 const productDelete = ['PRODUCT_DELETE']
 const productModify = ['PRODUCT_MODIFY']
 
 // 样衣管理
-const productSampleProductPage = ['PRODUCT_SAMPLEPRODUCT_PAGE']
+const productSampleProduct = ['PRODUCT_SAMPLEPRODUCT']
 const productSampleProductCreate = ['PRODUCT_SAMPLEPRODUCT_CREATE']
 
 // 产能配置（工厂）
-const factoryCapacityPage = ['FACTORY_CAPACITY_PAGE']
+const factoryCapacity = ['FACTORY_CAPACITY']
 const factoryCapacityModify = ['FACTORY_CAPACITY_MODIFY']
 const factoryCapacityPublish = ['FACTORY_CAPACITY_PUBLISH']
 
 // 主页
 
 // 认证中心
-const certPage = ['CERT_PAGE']
+const cert = ['CERT']
 
 // 员工
-const companyB2bCustomerPage = ['COMPANY_B2BCUSTOMER_PAGE']
+const companyB2bCustomer = ['COMPANY_B2BCUSTOMER']
 const companyB2bCustomerCreate = ['COMPANY_B2BCUSTOMER_CREATE']
 const companyB2bCustomerRemove = ['COMPANY_B2BCUSTOMER_REMOVE']
 const companyB2bCustomerModify = ['COMPANY_B2BCUSTOMER_MODIFY']
@@ -82,31 +84,32 @@ const companyB2bRoleRemove = ['COMPANY_B2BROLE_REMOVE']
 const companyB2bRoleModify = ['COMPANY_B2BROLE_MODIFY']
 
 // 合作商
-const companyCooperatorPage = ['COMPANY_COOPERATOR_PAGE']
+const companyCooperator = ['COMPANY_COOPERATOR']
 const companyCooperatorCreate = ['COMPANY_COOPERATOR_CREATE']
 const companyCooperatorRemove = ['COMPANY_COOPERATOR_REMOVE']
 const companyCooperatorModify = ['COMPANY_COOPERATOR_MODIFY']
 
 // 地址管理
-const companyAddressPage = ['COMPANY_ADDRESS_PAGE']
+const companyAddress = ['COMPANY_ADDRESS']
 const companyAddressCreate = ['COMPANY_ADDRESS_CREATE']
 const companyAddressRemove = ['COMPANY_ADDRESS_REMOVE']
 
 // 财务配置
-const factoryPayplanPage = ['FACTORY_PAYPLAN_PAGE']
+const factoryPayplan = ['FACTORY_PAYPLAN']
 const factoryPayplanCreate = ['FACTORY_PAYPLAN_CREATE']
-const factoryPayplanModify = ['FACTORY_PAYPLAN_Modify']
+const factoryPayplanModify = ['FACTORY_PAYPLAN_MODIFY']
 const factoryPayplanRemove = ['FACTORY_PAYPLAN_REMOVE']
 
 // 生产进度
-const reportProductionProgressPage = ['REPORT_PRODUCTION_PROGRESS_PAGE']
+const reportProductionProgress = ['REPORT_PRODUCTION_PROGRESS']
 
-// 钱包
-const companyWalletPage = ['COMPANY_WALLET_PAGE']
+// 其他
+const companyWallet = ['COMPANY_WALLET']
+const msgDelete = ['MSG_DELETE']
 
 export default {
   // 需求订单
-  requirementOrderPage,
+  requirementOrder,
   requirementOrderPublish,
   requirementOrderDetail,
   requirementOrderRecommended,
@@ -115,66 +118,68 @@ export default {
   requirementOrderModify,
 
   // 报价订单
-  qutoOrderPage,
-  qutoOrderDetail,
+  quoteOrder,
+  quoteOrderDetail,
   proofingOrderCreate,
   purchaseOrderCreate,
-  qutoOrderClose,
+  quoteOrderClose,
 
   // 打样订单
-  proofingOrderPage,
+  proofingOrder,
   proofingOrderView,
   proofingOrderDeliveryAddress,
   proofingOrderClose,
 
   // 生产订单
-  purchaseOrderPage,
+  purchaseOrder,
   purchaseOrderDetail,
   purchaseOrderOperate,
   purchaseOrderPayplanOp,
   purchaseOrderClose,
-  purchaseOrderAccept,
-  purchaseOrderReject,
   purchaseOrderOfflineCreate,
   purchaseOrderUniqueCodeImport,
+  purchaseOrderConfirmDelivering,
+  purchaseOrderConfirmReceived,
+  purchaseOrderConfirm,
+  purchaseOrderReconciliation,
 
   // 合同管理
-  agreementPage,
+  agreement,
   agreementSign,
 
   // 合同模板
-  agreementTmplPage,
+  agreementTmpl,
   agreementTmplCreate,
   agreementTmplView,
   agreementTmplDelete,
 
   // 签章管理
-  agreementSealPage,
+  agreementSeal,
   agreementSealCreate,
   agreementSealRemove,
 
   // 产品管理
-  productPage,
+  product,
   productCreate,
   productDelete,
   productModify,
 
   // 样衣管理
-  productSampleProductPage,
+  productSampleProduct,
   productSampleProductCreate,
 
   // 产能配置（工厂）
-  factoryCapacityPage,
+  factoryCapacity,
   factoryCapacityModify,
   factoryCapacityPublish,
 
   // 主页
 
   // 认证中心
-  certPage,
+  cert,
 
   // 员工
-  companyB2bCustomerPage,
+  companyB2bCustomer,
   companyB2bCustomerCreate,
   companyB2bCustomerRemove,
   companyB2bCustomerModify,
@@ -189,25 +194,26 @@ export default {
   companyB2bRoleModify,
 
   // 合作商
-  companyCooperatorPage,
+  companyCooperator,
   companyCooperatorCreate,
   companyCooperatorRemove,
   companyCooperatorModify,
 
   // 地址管理
-  companyAddressPage,
+  companyAddress,
   companyAddressCreate,
   companyAddressRemove,
 
   // 财务配置
-  factoryPayplanPage,
+  factoryPayplan,
   factoryPayplanCreate,
   factoryPayplanModify,
   factoryPayplanRemove,
 
   // 生产进度
-  reportProductionProgressPage,
+  reportProductionProgress,
 
-  // 钱包
-  companyWalletPage
+  // 其他
+  companyWallet,
+  msgDelete
 }
