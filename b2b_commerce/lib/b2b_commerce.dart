075d18b58ba585bbeeeb653fe6d26bc253509224
Konoha-orz <517136675@qq.com>
2020-04-02 20:14:40 +0800

    消息

--- b2b_commerce/lib/b2b_commerce.dart
+++ b2b_commerce/lib/b2b_commerce.dart
@@ -6,3 +6,4 @@ export 'src/business/orders/purchase_order_detail.dart';
 export 'src/business/orders/quote_order_detail.dart';
 export 'src/business/orders/requirement_order_detail.dart';
 export 'src/business/orders/requirement_order_detail_by_factory.dart';
+export 'src/business/orders/sale/sale_order_detail_page.dart';
