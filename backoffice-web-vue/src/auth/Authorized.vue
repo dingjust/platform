eae6146d618b0da719fdf9a3f82799346816a09c
Konoha-orz <517136675@qq.com>
2020-07-30 19:16:03 +0800

    权限校验

--- backoffice-web-vue/src/auth/Authorized.vue
+++ backoffice-web-vue/src/auth/Authorized.vue
@@ -1,27 +1,43 @@
-<script>
-import { check } from "./auth";
-import { Empty } from "./Empty";
+<template>
+  <div>
+    <template v-if="!hasPermission">
+      <div @click.stop="onCheck">
+        <slot></slot>
+      </div>
+    </template>
+    <template v-else>
+      <slot></slot>
+    </template>
+  </div>
+</template>
 
-export default {
-  functional: true,
-  components: {
-    Empty
-  },
-  props: {
-    authority: {
-      type: Array,
-      required: true
-    }
-  },
-  render(h, context) {
-    const { props, scopedSlots } = context;
-    // 如果校验通过，则返回子组件/插槽内容；否则返回null;
-    // return check(props.authority) ? scopedSlots.default() : null;
-    if (scopedSlots.default!=null ) {
-      return scopedSlots.default();
-    } else {
-      return Empty;
-    }
+<script>
+  export default {
+    name: 'Authorized',
+    props: {
+      permission: {
+        type: Array,
+        default: () => {
+          return [];
+        }
+      }
+    },
+    computed: {
+      hasPermission: function () {
+        return false;
+      }
+    },
+    methods: {
+      onCheck() {
+        this.$message('这是一条消息提示');
+        event.stopPropagation();
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
   }
-};
+
 </script>
