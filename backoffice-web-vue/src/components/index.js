fe8c269928f0c30b5306bb8080d93a693d165131
Konoha-orz <517136675@qq.com>
2020-09-04 16:21:12 +0800

    组件引用修改

--- backoffice-web-vue/src/components/index.js
+++ backoffice-web-vue/src/components/index.js
@@ -34,8 +34,7 @@ import PdfPreview from './custom/upload/PdfPreview'
 import Aside from './Aside.vue';
 import Breadcrumb from './Breadcrumb.vue';
 import Callout from './Callout.vue';
-import AppFooter from './Footer.vue';
-import AppHeader from './Header.vue';
+import AppHeader from './AppHeader.vue';
 import Sidebar from './Sidebar/Sidebar.vue';
 import Switch from './Switch.vue';
 import SideSlider from './SideSlider';
@@ -141,7 +140,6 @@ export {
   Aside,
   Breadcrumb,
   Callout,
-  AppFooter,
   AppHeader,
   Sidebar,
   Switch,
