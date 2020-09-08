5fdce80d7042c9e5bc531aaf0d6bdca331eab563
Konoha-orz <517136675@qq.com>
2020-09-08 17:07:12 +0800

    短信验证码接口 client_id修改===>nbyjy

--- services/lib/src/api/user.dart
+++ services/lib/src/api/user.dart
@@ -9,7 +9,7 @@ abstract class UserApis {
   static get sendCaptcha => (phone) => '/{baseSiteId}/sms/captcha/$phone';
   //发送登录用的验证码
   static get sendCaptchaForLogin =>
-          (phone) => '/{baseSiteId}/sms/captcha/asm/B2B/$phone';
+          (phone) => '/{baseSiteId}/sms/captcha/nbyjy/B2B/$phone';
 
   //验证验证码是否正确
   static get validateCaptcha => '/{baseSiteId}/sms/validate';
