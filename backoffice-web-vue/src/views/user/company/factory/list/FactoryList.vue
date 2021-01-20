e8f738363fafbc40a4e1961abeb94965636b7c3f
yi <13660339641@163.com>
2021-01-20 09:25:38 +0800

    平台端添加认证清除功能

--- backoffice-web-vue/src/views/user/company/factory/list/FactoryList.vue
+++ backoffice-web-vue/src/views/user/company/factory/list/FactoryList.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="animated fadeIn">
     <el-table v-if="isHeightComputed" ref="resultTable" stripe :data="page.content" :height="autoHeight">
-      <el-table-column label="商家编号" prop="uid" width="140"></el-table-column>
-      <el-table-column label="商家名称" prop="name" width="200"></el-table-column>
-      <el-table-column label="登录账号" prop="contactUid" width="140"></el-table-column>
-      <el-table-column label="联系人" prop="contactPerson" width="140"></el-table-column>
-      <el-table-column label="注册时间" prop="creationTime" width="140">
+      <el-table-column label="商家编号" prop="uid" min-width="110"></el-table-column>
+      <el-table-column label="商家名称" prop="name" min-width="200"></el-table-column>
+      <el-table-column label="登录账号" prop="contactUid" min-width="110"></el-table-column>
+      <el-table-column label="联系人" prop="contactPerson" min-width="110"></el-table-column>
+      <el-table-column label="注册时间" prop="creationTime" min-width="120">
         <template slot-scope="scope">
           <span>{{scope.row.creationTime | formatDate}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="标签" prop="labels">
+      <el-table-column label="标签" prop="labels" min-width="150">
         <template slot-scope="scope">
           <el-tag size="mini" class="disableTagClass" :disable-transitions="true" v-if="scope.row.loginDisabled">
             已禁用
           </el-tag>
           <el-tag v-for="(item, index) of showLabels(scope.row.labels, scope.row.approvalStatus)"
-                  size="mini" class="elTagClass" :disable-transitions="true">
+                  size="mini" class="elTagClass" :disable-transitions="true" :key="index">
             {{item}}
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="操作">
+      <el-table-column label="操作" min-width="200">
         <template slot-scope="scope">
           <slot name="operations" :item="scope.row"></slot>
         </template>
@@ -40,62 +40,56 @@
 </template>
 
 <script>
-    export default {
-      name: 'FactoryList',
-      props: ['page'],
-      computed: {
-      },
-      methods: {
-        showLabels (arr, approvalStatus) {
-          let arr1 = [];
-          if (approvalStatus != undefined && approvalStatus == 'approved') {
-            arr1[0] = '已认证';
-          } else {
-            arr1[0] = '未认证';
-          }
-          for (let i = 0; i < arr.length; i++) {
-            arr1[i + 1] = arr[i].name;
-          }
-          return arr1;
-        },
-        onPageSizeChanged (val) {
-          this._reset();
-          if (this.$store.state.FactoriesModule.isAdvancedSearch) {
-            this.$emit('onAdvancedSearch', val);
-            return;
-          }
-          this.$emit('onSearch', 0, val);
-        },
-        onCurrentPageChanged (val) {
-          if (this.$store.state.FactoriesModule.isAdvancedSearch) {
-            this.$emit('onAdvancedSearch', val - 1);
-            this.$nextTick(() => {
-              this.$refs.resultTable.bodyWrapper.scrollTop = 0
-            })
-            return;
-          }
-          this.$emit('onSearch', val - 1);
-          this.$nextTick(() => {
-            this.$refs.resultTable.bodyWrapper.scrollTop = 0
-          })
-        },
-        _reset () {
-          this.$refs.resultTable.clearSort();
-          this.$refs.resultTable.clearFilter();
-          this.$refs.resultTable.clearSelection();
-        },
-        onDetails (row) {
-          this.$emit('onDetails', row);
-        }
-      },
-      data () {
-        return {
-        }
-      },
-      created () {
+export default {
+  name: 'FactoryList',
+  props: ['page'],
+  computed: {
+  },
+  methods: {
+    showLabels (arr, approvalStatus) {
+      let arr1 = [];
+      if (approvalStatus != undefined && approvalStatus == 'approved') {
+        arr1[0] = '已认证';
+      } else {
+        arr1[0] = '未认证';
       }
+      for (let i = 0; i < arr.length; i++) {
+        arr1[i + 1] = arr[i].name;
+      }
+      return arr1;
+    },
+    onPageSizeChanged (val) {
+      this.$emit('onAdvancedSearch', 0, val);
+
+      this.$nextTick(() => {
+        this.$refs.resultTable.bodyWrapper.scrollTop = 0
+      })
+    },
+    onCurrentPageChanged (val) {
+      this.$emit('onAdvancedSearch', val - 1, 10);
+      
+      this.$nextTick(() => {
+        this.$refs.resultTable.bodyWrapper.scrollTop = 0
+      })
+    },
+    _reset () {
+      this.$refs.resultTable.clearSort();
+      this.$refs.resultTable.clearFilter();
+      this.$refs.resultTable.clearSelection();
+    },
+    onDetails (row) {
+      this.$emit('onDetails', row);
     }
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  }
+}
 </script>
+
 <style scoped>
   .disableTagClass{
     color: #0b0e0f;
--- backoffice-web-vue/src/views/user/company/factory/toolbar/FactoryToolbar.vue
+++ backoffice-web-vue/src/views/user/company/factory/toolbar/FactoryToolbar.vue
@@ -116,9 +116,9 @@
       <el-button style="background-color: #FFD60C" @click="onAdvancedSearch">搜索</el-button>
       <el-button @click="onReset">重置</el-button>
     </el-button-group>
-    <el-popover placement="bottom" width="800" trigger="click" ref="addressPopover" :width="200">
+    <el-popover placement="bottom" width="200" trigger="click" ref="addressPopover" >
       <el-form-item label="省" prop="productiveOrientations">
-        <el-select v-model="region" class="w-100" filterable @change="onRegionChanged">
+        <el-select v-model="region" style="width: 145px" filterable @change="onRegionChanged">
           <el-option v-for="item in regions"
                      :key="item.isocode"
                      :label="item.name"
@@ -127,7 +127,7 @@
         </el-select>
       </el-form-item>
       <el-form-item label="市" prop="cities">
-        <el-select class="w-100" v-model="queryFormData.cities" multiple filterable @change="recordCities">
+        <el-select style="width: 145px" v-model="queryFormData.cities" multiple filterable @change="recordCities">
           <el-option
             v-for="item in cities"
             :key="item.code"
@@ -141,169 +141,175 @@
 </template>
 
 <script>
-    import {createNamespacedHelpers} from 'vuex';
+import {createNamespacedHelpers} from 'vuex';
 
-    const {mapMutations} = createNamespacedHelpers('FactoriesModule');
+const {mapMutations} = createNamespacedHelpers('FactoriesModule');
 
-    export default {
-      name: 'FactoryToolbar',
-      props: ['hideCreationButton'],
-      components: {},
-      computed: {},
-      methods: {
-        ...mapMutations({
-          setKeyword: 'keyword',
-          setQueryFormData: 'queryFormData'
-        }),
-        onRegionChanged (current) {
-          if (!current || current == '') {
-            this.queryFormData.productiveOrientations = [];
-            this.queryFormData.cities = [];
-            this.cities = [];
-          }
-          if (this.queryFormData.productiveOrientations.length == 0) {
-            this.queryFormData.productiveOrientations.push(current);
-          } else {
-            this.queryFormData.productiveOrientations[0] = current;
-          }
-          this.cities = [];
-          this.getCities(current);
-
-          for (let i = 0; i < this.regions.length; i++) {
-            if (this.regions[i].isocode == this.region) {
-              this.regionText = this.regions[i].name + ':';
-            }
-          }
-          this.addressText = this.regionText;
-        },
-        async getCities (isocode) {
-          const url = this.apis().getCities(isocode);
-          const result = await this.$http.get(url);
-
-          if (result['errors']) {
-            this.$message.error(result['errors'][0].message);
-            return;
-          }
-
-          this.cities = result;
-        },
-        onSearch () {
-          this.setKeyword(this.keyword);
-          this.$emit('onSearch', 0);
-        },
-        onAdvancedSearch () {
-          if (this.queryFormData.starLevel == 0) {
-            this.queryFormData.starLevel = null;
-          }
-          this.setQueryFormData(this.queryFormData);
-          this.visible = false;
-          this.$emit('onAdvancedSearch', 0);
-        },
-        onNew () {
-          let formData = {};
-          Object.assign(formData, this.formData);
-
-          this.$emit('onNew', formData);
-        },
-        async getLabels () {
-          const url = this.apis().getGroupLabels('FACTORY');
-          const result = await this.$http.get(url);
-          if (result['errors']) {
-            this.$message.error(result['errors'][0].message);
-            return;
-          }
-
-          this.labels = result;
-        },
-        async getRegions () {
-          const url = this.apis().getRegions();
-          const result = await this.$http.get(url);
-          if (result['errors']) {
-            this.$message.error(result['errors'][0].message);
-            return;
-          }
-
-          this.regions = result;
-        },
-        async getCategories () {
-          const url = this.apis().getMajorCategories();
-          const result = await this.$http.get(url);
-          if (result['errors']) {
-            this.$message.error(result['errors'][0].message);
-            return;
-          }
-
-          this.categories = result;
-        },
-        async getMinorCategories () {
-          const url = this.apis().getMinorCategories();
-          const result = await this.$http.get(url);
-          if (result['errors']) {
-            this.$message.error(result['errors'][0].message);
-            return;
-          }
-
-          this.adeptAtCategories = result;
-        },
-        async getIndustrialClusters () {
-          const url = this.apis().getIndustrialClustersAll();
-          const result = await this.$http.get(url);
-          if (result['errors']) {
-            this.$message.error(result['errors'][0].message);
-            return;
-          }
-          this.industrialClusters = result;
-        },
-        recordCities (val) {
-          this.citiesText = '';
-          this.addressText = this.regionText;
-          for (let i = 0; i < this.cities.length; i++) {
-            for (let j = 0; j < val.length; j++) {
-              if (this.cities[i].code == val[j]) {
-                this.citiesText = this.citiesText + '/' + this.cities[i].name;
-              }
-            }
-          }
-          this.addressText = this.addressText + this.citiesText.substr(1);
-        },
-        onReset () {
-          this.queryFormData.keyword = '';
-          this.queryFormData.creationTimeFrom = '';
-          this.queryFormData.creationTimeTo = '';
-          this.queryFormData.adeptAtCategories = [];
-          this.queryFormData.cities = [];
-          this.region = '';
-          this.addressText = '';
-          this.queryFormData.productiveOrientations = [];
-        }
-      },
-      data () {
-        return {
-          visible: false,
-          keyword: '',
-          labels: [],
-          regions: [],
-          cities: [],
-          categories: [],
-          adeptAtCategories: [],
-          industrialClusters: [],
-          addressText: '',
-          citiesText: '',
-          regionText: '',
-          formData: this.$store.state.FactoriesModule.formData,
-          queryFormData: this.$store.state.FactoriesModule.queryFormData,
-          populationScales: this.$store.state.EnumsModule.populationScales,
-          starLevels: this.$store.state.EnumsModule.starLevels,
-          region: ''
-          // region:this.$store.state.FactoriesModule.formData.
-        }
-      },
-      created () {
-        this.getLabels();
-        this.getRegions();
-        this.getCategories();
-        this.getMinorCategories();
-        this.getIndustrialClusters();
+export default {
+  name: 'FactoryToolbar',
+  props: ['hideCreationButton'],
+  components: {},
+  computed: {},
+  methods: {
+    ...mapMutations({
+      setKeyword: 'keyword',
+      setQueryFormData: 'queryFormData'
+    }),
+    onRegionChanged (current) {
+      if (!current || current == '') {
+        this.queryFormData.productiveOrientations = [];
+        this.queryFormData.cities = [];
+        this.cities = [];
       }
+      if (this.queryFormData.productiveOrientations.length == 0) {
+        this.queryFormData.productiveOrientations.push(current);
+      } else {
+        this.queryFormData.productiveOrientations[0] = current;
+      }
+      this.cities = [];
+      this.getCities(current);
+
+      for (let i = 0; i < this.regions.length; i++) {
+        if (this.regions[i].isocode == this.region) {
+          this.regionText = this.regions[i].name + ':';
+        }
+      }
+      this.addressText = this.regionText;
+    },
+    async getCities (isocode) {
+      const url = this.apis().getCities(isocode);
+      const result = await this.$http.get(url);
+
+      if (result['errors']) {
+        this.$message.error(result['errors'][0].message);
+        return;
+      }
+
+      this.cities = result;
+    },
+    onSearch () {
+      this.setKeyword(this.keyword);
+      this.$emit('onSearch', 0);
+    },
+    onAdvancedSearch () {
+      if (this.queryFormData.starLevel == 0) {
+        this.queryFormData.starLevel = null;
+      }
+      this.setQueryFormData(this.queryFormData);
+      this.visible = false;
+      this.$emit('onAdvancedSearch', 0);
+    },
+    onNew () {
+      let formData = {};
+      Object.assign(formData, this.formData);
+
+      this.$emit('onNew', formData);
+    },
+    async getLabels () {
+      const url = this.apis().getGroupLabels('FACTORY');
+      const result = await this.$http.get(url);
+      if (result['errors']) {
+        this.$message.error(result['errors'][0].message);
+        return;
+      }
+
+      this.labels = result;
+    },
+    async getRegions () {
+      const url = this.apis().getRegions();
+      const result = await this.$http.get(url);
+      if (result['errors']) {
+        this.$message.error(result['errors'][0].message);
+        return;
+      }
+
+      this.regions = result;
+    },
+    async getCategories () {
+      const url = this.apis().getMajorCategories();
+      const result = await this.$http.get(url);
+      if (result['errors']) {
+        this.$message.error(result['errors'][0].message);
+        return;
+      }
+
+      this.categories = result;
+    },
+    async getMinorCategories () {
+      const url = this.apis().getMinorCategories();
+      const result = await this.$http.get(url);
+      if (result['errors']) {
+        this.$message.error(result['errors'][0].message);
+        return;
+      }
+
+      this.adeptAtCategories = result;
+    },
+    async getIndustrialClusters () {
+      const url = this.apis().getIndustrialClustersAll();
+      const result = await this.$http.get(url);
+      if (result['errors']) {
+        this.$message.error(result['errors'][0].message);
+        return;
+      }
+      this.industrialClusters = result;
+    },
+    recordCities (val) {
+      this.citiesText = '';
+      this.addressText = this.regionText;
+      for (let i = 0; i < this.cities.length; i++) {
+        for (let j = 0; j < val.length; j++) {
+          if (this.cities[i].code == val[j]) {
+            this.citiesText = this.citiesText + '/' + this.cities[i].name;
+          }
+        }
+      }
+      this.addressText = this.addressText + this.citiesText.substr(1);
+    },
+    onReset () {
+      this.queryFormData.keyword = '';
+      this.queryFormData.creationTimeFrom = '';
+      this.queryFormData.creationTimeTo = '';
+      this.queryFormData.adeptAtCategories = [];
+      this.queryFormData.cities = [];
+      this.region = '';
+      this.addressText = '';
+      this.queryFormData.productiveOrientations = [];
     }
+  },
+  data () {
+    return {
+      visible: false,
+      keyword: '',
+      labels: [],
+      regions: [],
+      cities: [],
+      categories: [],
+      adeptAtCategories: [],
+      industrialClusters: [],
+      addressText: '',
+      citiesText: '',
+      regionText: '',
+      formData: this.$store.state.FactoriesModule.formData,
+      queryFormData: this.$store.state.FactoriesModule.queryFormData,
+      populationScales: this.$store.state.EnumsModule.populationScales,
+      starLevels: this.$store.state.EnumsModule.starLevels,
+      region: ''
+      // region:this.$store.state.FactoriesModule.formData.
+    }
+  },
+  created () {
+    this.getLabels();
+    this.getRegions();
+    this.getCategories();
+    this.getMinorCategories();
+    this.getIndustrialClusters();
+  }
+}
 </script>
+
+<style scoped>
+  /deep/ .el-form-item {
+    margin-bottom: 0px;
+  }
+</style>
