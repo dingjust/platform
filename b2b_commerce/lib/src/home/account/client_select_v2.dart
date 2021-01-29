6029801ab2f046a61e3333b00c7313570c1c7a85
Konoha-orz <517136675@qq.com>
2021-01-29 14:49:32 +0800

    身份选择页v2

--- b2b_commerce/lib/src/home/account/client_select_v2.dart
+++ b2b_commerce/lib/src/home/account/client_select_v2.dart
@@ -11,11 +11,15 @@ class ClientSelectPageV2 extends StatefulWidget {
   _ClientSelectPageV2State createState() => _ClientSelectPageV2State();
 }
 
-class _ClientSelectPageV2State extends State<ClientSelectPageV2> {
+class _ClientSelectPageV2State extends State<ClientSelectPageV2>
+    with TickerProviderStateMixin {
   GlobalKey homePageKey = GlobalKey();
 
   ///全局Channel-原生通信
   static MethodChannel _globalChannel;
+
+  AnimationController _brandButtonController;
+  AnimationController _factoryButtonController;
 
   @override
   void initState() {
@@ -23,6 +27,16 @@ class _ClientSelectPageV2State extends State<ClientSelectPageV2> {
     _globalChannel = MethodChannel('net.nbyjy.b2b/global_channel');
     _globalChannel.setMethodCallHandler(_globalChannelMethodHandler);
     super.initState();
+    _brandButtonController = new AnimationController(
+        duration: new Duration(milliseconds: 400), vsync: this);
+    _brandButtonController.addStatusListener((status) {
+      setState(() {});
+    });
+    _factoryButtonController = new AnimationController(
+        duration: new Duration(milliseconds: 400), vsync: this);
+    _factoryButtonController.addStatusListener((status) {
+      setState(() {});
+    });
   }
 
   @override
@@ -30,42 +44,71 @@ class _ClientSelectPageV2State extends State<ClientSelectPageV2> {
     return Scaffold(
       key: homePageKey,
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    '选择您的身份',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
+          children: [
+            Expanded(
+                child: Builder(
+              builder: (bodyContext) => Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Align(
+                    alignment: Alignment(0, -0.5),
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 50),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '选择您的身份',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
+                  Align(
+                    alignment: Alignment(0, -0.2),
+                    child: LayoutBuilder(
+                        builder: (context, constraints) => Opacity(
+                              opacity:
+                                  _factoryButtonController.isAnimating ? 0 : 1,
+                              child: _Card(
+                                title: '品牌',
+                                text: '服装品牌商、贴牌贸易商等，需要寻找优质工厂或者服装款式的企业或个人',
+                                buttonController: _brandButtonController.view,
+                                maxHeight: constraints.maxHeight,
+                                onPressed: () {
+                                  UserBLoC.instance
+                                      .changeUserType(UserType.BRAND);
+                                },
+                              ),
+                            )),
+                  ),
+                  Align(
+                      alignment: Alignment(0, 0.2),
+                      child: LayoutBuilder(
+                          builder: (context, constraints) => Opacity(
+                                opacity:
+                                    _brandButtonController.isAnimating ? 0 : 1,
+                                child: _Card(
+                                  title: '工厂',
+                                  text: '服装生产工厂，需要寻找各类服装加工订单，管理生产进度的企业',
+                                  iconName: 'temp/index/select_factory.png',
+                                  buttonController:
+                                      _factoryButtonController.view,
+                                  maxHeight: constraints.maxHeight,
+                                  onPressed: () {
+                                    UserBLoC.instance
+                                        .changeUserType(UserType.FACTORY);
+                                  },
+                                ),
+                              )))
                 ],
               ),
-            ),
-            _Card(
-              title: '品牌',
-              text: '服装品牌商、贴牌贸易商等，需要寻找优质工厂或者服装款式的企业或个人',
-              margin: EdgeInsets.symmetric(vertical: 40),
-              onPressed: () {
-                UserBLoC.instance.changeUserType(UserType.BRAND);
-              },
-            ),
-            _Card(
-              title: '工厂',
-              text: '服装生产工厂，需要寻找各类服装加工订单，管理生产进度的企业',
-              iconName: 'temp/index/select_factory.png',
-              onPressed: () {
-                UserBLoC.instance.changeUserType(UserType.FACTORY);
-              },
-            )
+            ))
           ],
         ),
       ),
@@ -91,6 +134,8 @@ class _ClientSelectPageV2State extends State<ClientSelectPageV2> {
   @override
   void dispose() {
     _globalChannel = null;
+    _brandButtonController.dispose();
+    _factoryButtonController.dispose();
     super.dispose();
   }
 }
@@ -110,33 +155,61 @@ class _Card extends StatelessWidget {
 
   final VoidCallback onPressed;
 
-  const _Card({Key key,
+  final AnimationController buttonController;
+
+  final Animation buttonSqueezeanimation;
+
+  final double maxHeight;
+
+  _Card({Key key,
     this.height = 120,
     this.title,
     this.text,
     this.margin = EdgeInsets.zero,
     this.iconName = 'temp/index/select_brand.png',
     this.iconPackage = 'assets',
-    this.onPressed})
-      : super(key: key);
+    this.onPressed,
+    this.buttonController,
+    this.maxHeight = 1080})
+      : buttonSqueezeanimation = Tween(
+    begin: height,
+    end: maxHeight,
+  ).animate(
+    new CurvedAnimation(
+        parent: buttonController, curve: Curves.easeInQuad),
+  ),
+        super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
+  Future<Null> _playAnimation() async {
+    try {
+      await buttonController.forward().then((value) {
+        if (onPressed != null) {
+          onPressed.call();
+        }
+      });
+      // await buttonController.reverse();
+    } on TickerCanceled {}
+  }
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    bool isAnimated = buttonSqueezeanimation.value > height;
+
     return Container(
       margin: margin,
+      height: buttonSqueezeanimation.value,
+      padding: EdgeInsets.symmetric(horizontal: isAnimated ? 0 : 10),
       child: Material(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
         child: InkWell(
           onTap: () {
-            if (onPressed != null) {
-              onPressed.call();
-            }
+            _playAnimation();
           },
           borderRadius: BorderRadius.circular(10),
           child: Container(
+            margin: EdgeInsets.symmetric(horizontal: isAnimated ? 10 : 0),
             height: height,
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
@@ -150,28 +223,31 @@ class _Card extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 5),
-                        child: Text(
-                          '$title',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 21),
-                          textAlign: TextAlign.start,
+                  child: Container(
+                    height: height - 30,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(bottom: 5),
+                          child: Text(
+                            '$title',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 21),
+                            textAlign: TextAlign.start,
+                          ),
                         ),
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                              child: Text(
-                                '$text',
-                                style: TextStyle(color: Colors.grey),
-                              ))
-                        ],
-                      )
-                    ],
+                        Row(
+                          children: [
+                            Expanded(
+                                child: Text(
+                                  '$text',
+                                  style: TextStyle(color: Colors.grey),
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Icon(
@@ -184,6 +260,14 @@ class _Card extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: buttonController,
     );
   }
 }
