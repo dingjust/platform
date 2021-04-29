81654019b61807d78f58b5bf14b79f364801a799
Konoha-orz <517136675@qq.com>
2021-04-29 15:48:31 +0800

    极光推送集成-IOS

--- b2b_commerce/ios/Runner/AppDelegate.m
+++ b2b_commerce/ios/Runner/AppDelegate.m
@@ -3,6 +3,14 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 #import <UMCommon/UMConfigure.h>
+#import "JPUSHService.h"
+#import <UserNotifications/UserNotifications.h>
+
+#define APPKEY @"824c12a64440fbf70e767a3e"
+
+@interface AppDelegate ()<JPUSHRegisterDelegate>
+
+@end
 
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application
@@ -13,6 +21,19 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 //    NSString * deviceId=[UMConfigure deviceIDForIntegration];
 //    NSLog(@"集成测试：%@",deviceId);
     
+    //极光推送
+    //注册通知 通知回调代理
+    JPUSHRegisterEntity * entity=[[JPUSHRegisterEntity alloc]init];
+    if (@available(iOS 12.0, *)) {
+        entity.types=JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings;
+    } else {
+        // Fallback on earlier versions
+    }
+    [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+    //初始化SDK
+    [JPUSHService setupWithOption:launchOptions appKey:APPKEY channel:@"App Store" apsForProduction:YES advertisingIdentifier:nil];
+    
+    
     [GeneratedPluginRegistrant registerWithRegistry:self];
     // Override point for customization after application launch.
     if(![[NSUserDefaults standardUserDefaults]objectForKey:@"Notification"]){
@@ -21,6 +42,8 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     }
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+
 //- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
 //    NSLog(@"[NBYJY]=========>%@",url);
 //    return NO;
@@ -53,4 +76,56 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 //}
 //- (void)applicationWillTerminate:(UIApplication *)application {
 //}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    //sdk注册DeviceToken
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application
+    didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+
+#pragma mark- JPUSHRegisterDelegate
+//设置消息送达代理
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger options))completionHandler {
+    NSDictionary * userInfo = notification.request.content.userInfo;
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionSound|UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以设置
+}
+
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)(void))completionHandler {
+  
+    NSDictionary * userInfo = response.notification.request.content.userInfo;
+    
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler();  // 系统要求执行这个方法
+}
+
+- (void)jpushNotificationAuthorization:(JPAuthorizationStatus)status withInfo:(NSDictionary *)info {
+    NSLog(@"receive notification authorization status:%lu, info:%@", (unsigned long)status, info);
+}
+
+#ifdef __IPHONE_12_0
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center openSettingsForNotification:(UNNotification *)notification {
+    NSString *title = nil;
+    if (notification) {
+        title = @"从通知界面直接进入应用";
+    }else{
+        title = @"从系统设置界面进入应用";
+    }
+    NSLog(@"%@", title);
+}
+#endif
+
+//【iOS7以上系统，收到通知以及静默推送】
+- (void)application:(UIApplication *)application
+    didReceiveRemoteNotification:(NSDictionary *)userInfo
+          fetchCompletionHandler:
+              (void (^)(UIBackgroundFetchResult))completionHandler {
+  [JPUSHService handleRemoteNotification:userInfo];
+  completionHandler(UIBackgroundFetchResultNewData);
+}
+
 @end
