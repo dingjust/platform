8ca6e4614b6b0569b67c40b40929099e50709d67
Konoha-orz <517136675@qq.com>
2021-07-07 11:10:38 +0800

    数据空显示

--- b2b_commerce/lib/src/_shared/widgets/empty.dart
+++ b2b_commerce/lib/src/_shared/widgets/empty.dart
@@ -7,19 +7,20 @@ class Empty extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         color: Colors.white,
-        child: Center(
-            child: Column(
+        child: ListView(
           children: [
             Image.asset(
               'temp/empty-image-search.png',
               package: 'assets',
-              fit: BoxFit.fitWidth,
+              width: 200,
+              height: 200,
             ),
             Text(
               '没找到相关内容',
               style: TextStyle(color: Colors.grey, fontSize: 12),
+              textAlign: TextAlign.center,
             )
           ],
-        )));
+        ));
   }
 }
