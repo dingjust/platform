24262baa4003279650d3a9ebbeb4a11be794d8a0
Konoha-orz <517136675@qq.com>
2021-08-20 16:19:43 +0800

    1.9.2

--- core/lib/src/config/configs.dart
+++ core/lib/src/config/configs.dart
@@ -10,8 +10,8 @@ class GlobalConfigs {
   // static const MEDIA_CONTEXT_PATH = 'http://sc.nbyjy.net';
 
   //本地
-  // static const CONTEXT_PATH = 'https://192.168.1.179:9002/djwebservices/v2';
-  // static const MEDIA_CONTEXT_PATH = 'http://192.168.1.179';
+  // static const CONTEXT_PATH = 'https://192.168.1.177:9002/djwebservices/v2';
+  // static const MEDIA_CONTEXT_PATH = 'http://192.168.1.177';
 
   // 生产
   static const CONTEXT_PATH = 'https://ht.nbyjy.net';
@@ -37,7 +37,7 @@ class GlobalConfigs {
   static const AUTH_SERVER_URL = '$CONTEXT_PATH/authorizationserver';
 
   // static const AUTH_SERVER_URL =
-  //     'https://192.168.1.179:9002/authorizationserver';
+  //     'https://192.168.1.177:9002/authorizationserver';
 
   ///websocket 连接
   // static get WEBSOCKET_URL => (String token, String uid) {
