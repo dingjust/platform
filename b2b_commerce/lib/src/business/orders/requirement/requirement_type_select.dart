20c12608e22bc4002cf04664b94fa57ff4050682
Konoha-orz <517136675@qq.com>
2021-08-28 14:38:09 +0800

    发布需求修改

--- b2b_commerce/lib/src/business/orders/requirement/requirement_type_select.dart
+++ b2b_commerce/lib/src/business/orders/requirement/requirement_type_select.dart
@@ -1,3 +1,4 @@
+import 'package:b2b_commerce/src/_shared/widgets/app_bar_factory.dart';
 import 'package:flutter/material.dart';
 import 'package:models/models.dart';
 
@@ -9,18 +10,7 @@ class RequirementTypeSelectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-        iconTheme: IconThemeData(color: Colors.black),
-        centerTitle: true,
-        title: Text(
-          '需求发布',
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBarFactory.buildDefaultAppBar('需求发布'),
       body: Row(
         children: [
           Expanded(
@@ -32,68 +22,68 @@ class RequirementTypeSelectPage extends StatelessWidget {
                   onTap: () =>
                       toNext(context, RequirementOrderType.FINDING_FACTORY),
                   child: Container(
-                    margin: EdgeInsets.only(top: 52, bottom: 84),
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          'temp/index/find_factory.png',
-                          package: 'assets',
-                          fit: BoxFit.fitWidth,
-                          width: 148,
-                          height: 148,
+                        margin: EdgeInsets.only(top: 52, bottom: 84),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'temp/index/find_factory.png',
+                              package: 'assets',
+                              fit: BoxFit.fitWidth,
+                              width: 148,
+                              height: 148,
+                            ),
+                            Container(
+                                margin: EdgeInsets.only(top: 12),
+                                width: 160,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                    color: Color(0xFFFED800),
+                                    borderRadius: BorderRadius.circular(24)),
+                                child: Center(
+                                  child: Text(
+                                    '我要找工厂',
+                                    style: TextStyle(fontSize: 15),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ))
+                          ],
                         ),
-                        Container(
-                            margin: EdgeInsets.only(top: 12),
-                            width: 160,
-                            height: 48,
-                            decoration: BoxDecoration(
-                                color: Color(0xFFFED800),
-                                borderRadius: BorderRadius.circular(24)),
-                            child: Center(
-                              child: Text(
-                                '我要找工厂',
-                                style: TextStyle(fontSize: 15),
-                                textAlign: TextAlign.center,
-                              ),
-                            ))
-                      ],
+                      ),
                     ),
-                  ),
+                    GestureDetector(
+                      onTap: () =>
+                          toNext(context, RequirementOrderType.FINDING_ORDER),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'temp/index/find_order.png',
+                              package: 'assets',
+                              fit: BoxFit.fitWidth,
+                              width: 148,
+                              height: 148,
+                            ),
+                            Container(
+                                margin: EdgeInsets.only(top: 12),
+                                width: 160,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                    color: Color(0xFFFED800),
+                                    borderRadius: BorderRadius.circular(24)),
+                                child: Center(
+                                  child: Text(
+                                    '我要抢订单',
+                                    style: TextStyle(fontSize: 15),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                GestureDetector(
-                  onTap: () =>
-                      toNext(context, RequirementOrderType.FINDING_ORDER),
-                  child: Container(
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          'temp/index/find_order.png',
-                          package: 'assets',
-                          fit: BoxFit.fitWidth,
-                          width: 148,
-                          height: 148,
-                        ),
-                        Container(
-                            margin: EdgeInsets.only(top: 12),
-                            width: 160,
-                            height: 48,
-                            decoration: BoxDecoration(
-                                color: Color(0xFFFED800),
-                                borderRadius: BorderRadius.circular(24)),
-                            child: Center(
-                              child: Text(
-                                '我要抢订单',
-                                style: TextStyle(fontSize: 15),
-                                textAlign: TextAlign.center,
-                              ),
-                            ))
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ))
+              ))
         ],
       ),
     );
