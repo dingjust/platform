2b499823e8c9d117b9660a76db2a6a35f56cc9de
ky <614110832@qq.com>
2021-09-15 17:41:50 +0800

    修改公司介绍报错，清楚个人认证添加强制清楚

--- backoffice-web-vue/src/views/user/company/components/AuthenticationClearForm.vue
+++ backoffice-web-vue/src/views/user/company/components/AuthenticationClearForm.vue
@@ -20,7 +20,7 @@
       <el-radio label="ENTERPRISE" v-model="selection">{{options.ENTERPRISE}}</el-radio>
     </el-row>
     <collapse-transition>
-      <div class="collapse-wrap" v-show="showSelection">
+      <div class="collapse-wrap" v-show="showComSelection">
         <slot>
           <div class="basic-row">
             <h6>是否强制清除，已经认证的公司必须强制清除</h6>
@@ -40,6 +40,19 @@
     <el-row type="flex">
       <el-radio label="PERSON" v-model="selection">{{options.PERSON}}</el-radio>
     </el-row>
+    <collapse-transition>
+      <div class="collapse-wrap" v-show="showPerSelection">
+        <slot>
+          <div class="basic-row">
+            <h6>是否强制清除，已经认证的公司必须强制清除</h6>
+            <el-radio-group v-model="personData.forceClean">
+              <el-radio :label="true">是</el-radio>
+              <el-radio :label="false" :disabled="approvalStatus === 'approved'">否</el-radio>
+            </el-radio-group>
+          </div>
+        </slot>
+      </div> 
+    </collapse-transition>
     <el-row type="flex" justify="end">
       <el-button @click="onCancel">取消</el-button>
       <el-button type="primary" @click="onConfirm">清除</el-button>
@@ -69,10 +82,14 @@ export default {
   data () {
     return {
       selection: '',
-      showSelection: false,
+      showComSelection: false,
+      showPerSelection: false,
       enterpriseData: {
         forceClean: false,
         recreateCustomerId: false
+      },
+      personData: {
+        forceClean: false
       },
       options: {
         DELETE: '删除认证',
@@ -102,9 +119,16 @@ export default {
                                             : h('p', {style: 'margin-left: 20px'}, '强制清除 ❌'),
             this.enterpriseData.recreateCustomerId ? h('p', {style: 'margin-left: 20px'}, '重新生成customerId ✔️') 
                                                     : h('p', {style: 'margin-left: 20px'}, '重新生成customerId ❌')
-          ]) : h('div', {style: 'margin-left: 11px'}, [
+          ]) :  (
+            this.selection === 'PERSON' ? h('div', {style: 'margin-left: 11px'}, [
+            h('p', {style: 'margin-top: 3px'}, this.confirmText),
+            this.enterpriseData.forceClean ? h('p', {style: 'margin-left: 20px'}, '强制清除 ✔️') 
+                                            : h('p', {style: 'margin-left: 20px'}, '强制清除 ❌')
+            ]) :
+            h('div', {style: 'margin-left: 11px'}, [
             h('p', {style: 'margin-top: 3px'}, this.confirmText)
           ])
+          )
         ]),
         showCancelButton: true,
         confirmButtonText: '确定',
@@ -173,7 +197,9 @@ export default {
       const contactUid = this.clearRow.contactUid;
 
       const url = this.apis().clearPersonAuth(contactUid);
-      const result = await this.$http.post(url);
+      const result = await this.$http.post(url, {}, {
+        forceClean: this.personData.forceClean
+      });
 
       if (result['errors']) {
         this.$message.error(result['errors'][0].message ? result['errors'][0].message : '操作失败，请稍后再试');
@@ -193,15 +219,21 @@ export default {
   watch: {
     'selection': function (nval, ovla) {
       if (nval && nval === 'ENTERPRISE') {
-        this.showSelection = true;
+        this.showComSelection = true;
+        this.showPerSelection = false;
+      } else if (nval && nval === 'PERSON') {
+        this.showComSelection = false;
+        this.showPerSelection = true;
       } else {
-        this.showSelection = false;
+        this.showComSelection = false;
+        this.showComSelection = false;
       }
     }
   },
   mounted () {
     if (this.approvalStatus) {
       this.enterpriseData.forceClean = true;
+      this.personData.forceClean = true;
     }
   }
 }
