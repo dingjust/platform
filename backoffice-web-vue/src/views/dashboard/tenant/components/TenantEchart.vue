e7378dfbdadb43af1c1cc63960b3a1148f44dbed
ky <614110832@qq.com>
2021-09-28 17:11:46 +0800

    修改平台端统计图表

--- backoffice-web-vue/src/views/dashboard/tenant/components/TenantEchart.vue
+++ backoffice-web-vue/src/views/dashboard/tenant/components/TenantEchart.vue
@@ -2,28 +2,32 @@
 * @Description: 平台端首页图表模板
 * @Date 2021/07/02 20:31
 * @Author L.G.Y
+* 
+* unit 枚举 0 = 日，1 = 年，2 = 月， （3 = 周不作为后端接口参数，为前端自行处理
 -->
 <template>
   <div class="tenant-echart">
     <div class="echart-header">
       <h6>{{chartData.name}}</h6>
-      <!-- <el-select v-model="unit" placeholder="请选择" style="width: 60px" @change="handleType">
+      <el-select v-model="unit" placeholder="请选择" style="width: 60px" @change="handleType">
         <el-option label="日" :value="0" :key="0"/>
+        <!-- <el-option label="年" :value="1" :key="1"/> -->
+        <el-option label="周" :value="3" :key="3"/>
         <el-option label="月" :value="2" :key="2"/>
       </el-select>
       <el-date-picker
-        v-model="time"
+        v-model="chartData.time"
         @change="handleChange"
         value-format="yyyy/MM/dd 00:00:00"
         style="width: 270px"
-        :type="unit === 0 ? 'daterange' : 'monthrange'"
+        :type="unit === 2 ? 'monthrange' : 'daterange'"
         unlink-panels
         range-separator="至"
         start-placeholder="开始日期"
         end-placeholder="结束日期"
         :picker-options="pickerOptions">
       </el-date-picker>
-      <el-button type="text" @click="downloadDetail">下载明细</el-button> -->
+      <el-button type="text" @click="downloadDetail">下载明细</el-button>
     </div>
     <div v-loading="chartData.loading">
       <div :id="main" :style="'width: 500px;height:300px'"></div>
@@ -38,19 +42,20 @@ export default {
   props: ['chartData', 'code', 'defaultTime'],
   methods: {
     handleType (value) {
-      const start = new Date(this.time[0])
-      const end = new Date(this.time[1])
+      const start = new Date(this.chartData.time[0])
+      const end = new Date(this.chartData.time[1])
       // 月->日
-      if (value === 0) {
+      if (this.oldUnit === 2 && value !== 2) {
         let startT = this.handleTime(new Date(`${start.getFullYear()}-${start.getMonth() + 1}-01`));
 
         const endDate = new Date(`${end.getFullYear()}-${end.getMonth() + 1}-01`);
         let endT = this.handleTime(endDate.setMonth(endDate.getMonth() + 1) - 24*60*60*1000)
 
-        this.$set(this, 'time', [startT, endT])
-      } 
+        this.$set(this.chartData, 'time', [startT, endT])
+      }
     
-      this.handleChange(this.time)
+      this.oldUnit = this.unit
+      this.handleChange(this.chartData.time)
     },
     handleChange (value) {
       let params = {
@@ -120,12 +125,55 @@ export default {
   data () {
     return {
       main: null,
-      time: [],
       unit: 0,
+      oldUnit: 0,
       pickerOptions: {
         disabledDate(time) {
           return time.getTime() > Date.now();
-        }
+        },
+        shortcuts: [
+          {
+            text: '最近一周',
+            onClick(picker) {
+              const start = new Date()
+              const end = new Date()
+              
+              start.setDate(start.getDate() - start.getDay())
+              end.setDate(end.getDate() + (7 - end.getDay() - 1))
+
+              picker.$emit('pick', [start, end]);
+            }
+          }, 
+          {
+            text: '最近一个月',
+            onClick(picker) {
+              const start = new Date();
+              const end = new Date();
+
+              start.setDate(1);
+
+              end.setDate(1)
+              const targetEnd = new Date(end.setMonth(end.getMonth() + 1) - 24*60*60*1000)
+              
+              picker.$emit('pick', [start, targetEnd]);
+            }
+          }, 
+          {
+            text: '最近三个月',
+            onClick(picker) {
+              const start = new Date();
+              const end = new Date();
+
+              start.setDate(1);
+              const targetStart = new Date(start.setMonth(start.getMonth() - 2))
+
+              end.setDate(1)
+              const targetEnd = new Date(end.setMonth(end.getMonth() + 1) - 24*60*60*1000)
+              console.log(targetStart, targetEnd)
+              picker.$emit('pick', [targetStart, targetEnd]);
+            }
+          }
+        ]
       },
     }
   },
@@ -140,11 +188,6 @@ export default {
   },
   created () {
     this.$set(this, 'main', this.code)
-
-    let endDate = new Date(this.defaultTime.end)
-
-    this.time[0] = this.defaultTime.start
-    this.time[1] = this.handleTime(endDate.setDate(endDate.getDate() - 1))
   }
 }
 </script>
--- backoffice-web-vue/src/views/dashboard/tenant/components/TenantEchartTab.vue
+++ backoffice-web-vue/src/views/dashboard/tenant/components/TenantEchartTab.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="echart-cell">
     <template v-for="(value, key) in echartArr">
-      <tenant-echart :key="key" :code="key" :chartData="value" :defaultTime="defaultTime" @getEchartData="getEchartData" @handleTime="handleTime"/>
+      <tenant-echart :key="key" :code="key" :chartData="value" @getEchartData="getEchartData" @handleTime="handleTime"/>
     </template>
   </div>
 </template>
@@ -9,8 +9,6 @@
 <script>
 import { formatDate } from '@/common/js/filters';
 import TenantEchart from './TenantEchart'
-
-import { echartData } from './echart'
 
 export default {
   name: 'TenantEchartTab',
@@ -21,17 +19,12 @@ export default {
     },
     init() {
       const date = new Date();
+
       const end = new Date(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`).getTime() + 24*60*60*1000;
       const start = end - 7*24*60*60*1000
 
-      this.$set(this, 'defaultTime', {
-        start: this.handleTime(start), 
-        end: this.handleTime(end)
-      })
-
       for (const key in this.echartArr) {
         if (Object.hasOwnProperty.call(this.echartArr, key)) {
-          const element = this.echartArr[key];
           this.getEchartData(key, 0, {
             start: this.handleTime(start), 
             end: this.handleTime(end)
@@ -40,55 +33,113 @@ export default {
       }
     },
     async getEchartData (type, unit, params) {
-      this.echartArr[type].loading = true 
+      this.echartArr[type].loading = true
+      
+      let week = false
+
+      // 查询 周为单位时，需处理查询时间，保证start,end分别为 周日， 周六
+      if (unit === 3) {
+        let start = new Date(params.start)
+        let end = new Date(params.end)
+
+        start.setDate(start.getDate() - start.getDay())
+        // 组件上处理end时间加了1天，所以这里判断是否为 0，不判断是否为 6
+        end.getDay() === 0 || end.setDate(end.getDate() + (7 - end.getDay()))
+
+        params.start = this.handleTime(start)
+        params.end = this.handleTime(end)
+
+        unit = 0
+        week = true
+      }
+
       const url = this.apis().getEchartData(type, unit);
       const result = await this.$http.post(url, {}, params, true)
 
       if (result.code === 1) {
         this.echartArr[type].loading = false
-        // this.echartArr[type].data = result.data
-        this.echartArr[type].data = echartData[type].data
+        this.echartArr[type].data = this.handleResult(result.data, type, week, params)
+
+        this.echartArr[type].time = [params.start, this.handleTime(new Date(params.end).getTime() - 24*60*60*1000)]
       }
     },
+    handleResult (data, type, week) {
+      let result = []
+      
+      if (week) {
+        this.handleWeek(data, type, 0, result)
+      } else {
+        result = data
+      }
+
+      return result
+    },
+    handleWeek (data, type, index, result) {
+      let item = {
+        currentDate: data[index].currentDate,
+        total: 0,
+        amount: 0
+      }
+      
+      data.slice(index, index += 7).forEach(val => {
+        if (type === 'salesAmount') {
+          item.amount += val.amount
+        } else {
+          item.total += val.total
+        }
+      })
+
+      result.push(item)
+
+      if (index < data.length) {
+        this.handleWeek(data, type, index, result)
+      }
+    }
   },
   data () {
     return {
-      defaultTime: null,
       echartArr: {
         agreement: {
           name: '签约数',
           loading: false,
-          data: []
+          data: [],
+          time: []
         },
         quote: {
           name: '报价数',
           loading: false,
-          data: []
+          data: [],
+          time: []
         },
         requirement: {
           name: '需求数',
           loading: false,
-          data: []
+          data: [],
+          time: []
         },
         sales: {
           name: '销售订单',
           loading: false,
-          data: []
+          data: [],
+          time: []
         },
         salesAmount: {
           name: '销售额',
           loading: false,
-          data: []
+          data: [],
+          time: []
         },
         salesQuantity: {
           name: '生产量',
           loading: false,
-          data: []
+          data: [],
+          time: []
         },
         register: {
           name: '注册用户数',
           loading: false,
-          data: []
+          data: [],
+          time: []
         }
       }
     }
