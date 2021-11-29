3c62efaed196e0e99e4821c58da6ca6b72ac18d8
Konoha-orz <517136675@qq.com>
2021-11-29 09:52:10 +0800

    导出修改

--- backoffice-web-vue/src/views/order/pend-out-view/PendOutView.vue
+++ backoffice-web-vue/src/views/order/pend-out-view/PendOutView.vue
@@ -12,7 +12,8 @@
         </div>
       </el-row>
       <div class="pt-2"></div>
-      <pend-out-view-toolbar :queryFormData="queryFormData" @onAdvancedSearch="onAdvancedSearch" @onExport="onExport" @onExportByBill="onExportByBill"/>
+      <pend-out-view-toolbar :queryFormData="queryFormData" @onAdvancedSearch="onAdvancedSearch" @onExport="onExport"
+        @onExportByBill="onExportByBill" />
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <template v-for="item in statuses">
           <el-tab-pane :label="item.name" :name="item.code" :key="item.code">
@@ -25,124 +26,143 @@
 </template>
 
 <script>
-import {
-  createNamespacedHelpers
-} from 'vuex';
+  import {
+    createNamespacedHelpers
+  } from 'vuex';
 
-const {
-  mapGetters,
-  mapActions,
-} = createNamespacedHelpers(
-  'PendOutOrderModule'
-);
+  const {
+    mapGetters,
+    mapActions,
+  } = createNamespacedHelpers(
+    'PendOutOrderModule'
+  );
 
-import PendOutViewToolbar from './toolbar/PendOutViewToolbar'
-import PendOutViewList from './list/PendOutViewList'
+  import PendOutViewToolbar from './toolbar/PendOutViewToolbar'
+  import PendOutViewList from './list/PendOutViewList'
 
-import { formatDate } from '@/common/js/filters';
-import { exportTable } from '@/common/js/export'
+  import {
+    formatDate
+  } from '@/common/js/filters';
+  import {
+    exportTable
+  } from '@/common/js/export'
 
-export default {
-  name: 'PendOutView',
-  components: { PendOutViewToolbar, PendOutViewList },
-  computed: {
-    ...mapGetters({
-      page: 'page',
-      keyword: 'keyword',
-      queryFormData: 'queryFormData'
-    }),
-  },
-  methods: {
-    ...mapActions({
-      searchAdvanced: 'searchAdvanced'
-    }), 
-    onAdvancedSearch(page, size) {
-      const query = this.queryFormData;
+  import {
+    accMul
+  } from '@/common/js/number';
 
-      const url = this.apis().searchPendOut();
-      this.searchAdvanced({url, query, page, size});
+
+  export default {
+    name: 'PendOutView',
+    components: {
+      PendOutViewToolbar,
+      PendOutViewList
     },
-    handleClick(tab, event) {
-      this.queryFormData.state = tab.name;
-      this.onAdvancedSearch(0, this.page.size);
+    computed: {
+      ...mapGetters({
+        page: 'page',
+        keyword: 'keyword',
+        queryFormData: 'queryFormData'
+      }),
     },
-    async onExport () {
-      const totalElements = this.page.totalElements
+    methods: {
+      ...mapActions({
+        searchAdvanced: 'searchAdvanced'
+      }),
+      onAdvancedSearch(page, size) {
+        const query = this.queryFormData;
 
-      const exportData = await this.getExportData(totalElements)
-      exportTable(exportData, '交易订单')
-    },
-    async getExportData (totalElements) {
-      const query = this.queryFormData;
+        const url = this.apis().searchPendOut();
+        this.searchAdvanced({
+          url,
+          query,
+          page,
+          size
+        });
+      },
+      handleClick(tab, event) {
+        this.queryFormData.state = tab.name;
+        this.onAdvancedSearch(0, this.page.size);
+      },
+      async onExport() {
+        const totalElements = this.page.totalElements
 
-      const url = this.apis().searchPendOut();
-      const result = await this.$http.post(url, query, {
-        page: 0,
-        size: totalElements
-      })
+        const exportData = await this.getExportData(totalElements)
+        exportTable(exportData, '交易订单')
+      },
+      async getExportData(totalElements) {
+        const query = this.queryFormData;
 
-      let exportData = [];
-      exportData = result.content.map(e => {
-        return {
-          订单号: e.code,
-          甲方公司: e.originCooperator ? e.originCooperator.name : '',
-          乙方公司: e.belongTo ? e.belongTo.name : '',
-          款数: e.entrySize ? e.entrySize : '',
-          订单数量: e.totalQuantity ? e.totalQuantity : '',
-          服务费比例: e.serviceFeePercent ? (e.serviceFeePercent + '%') : '',
-          订单金额: e.totalPrimeCost ? e.totalPrimeCost : '',
-          结算金额: e.onlinePaidAmount ? e.onlinePaidAmount : '',
-          创建时间: e.creationtime ? formatDate(new Date(e.creationtime), 'yyyy-MM-dd hh:mm:ss') : '',
-          订单状态: this.getEnum('SalesProductionOrderState', e.state)
-        }
-      })
+        const url = this.apis().searchPendOut();
+        const result = await this.$http.post(url, query, {
+          page: 0,
+          size: totalElements
+        })
 
-      return exportData
-    },
-    async onExportByBill () {
-      const totalElements = this.page.totalElements
-
-      const exportData = await this.getExportDataByBill(totalElements)
-      exportTable(exportData, '交易流水订单')
-    },
-    async getExportDataByBill (totalElements) {
-      const query = this.queryFormData;
-
-      const url = this.apis().searchPendOut();
-      const result = await this.$http.post(url, query, {
-        page: 0,
-        size: totalElements
-      })
-
-      let exportData = [];
-      result.content.filter(item => item.paymentOrders && item.paymentOrders.length > 0).forEach(e => {
-        e.paymentOrders.filter(i => i.state === 'PAID').forEach(paymentOrder => {
-          exportData.push({
+        let exportData = [];
+        exportData = result.content.map(e => {
+          return {
             订单号: e.code,
-            流水单号: paymentOrder.outOrderNo,
-            流水金额: paymentOrder.totalAmount ? paymentOrder.totalAmount : paymentOrder.payAmount,
-            流水支付时间: paymentOrder.paySuccessTime ? formatDate(new Date(paymentOrder.paySuccessTime), 'yyyy-MM-dd hh:mm:ss') : '',
             甲方公司: e.originCooperator ? e.originCooperator.name : '',
             乙方公司: e.belongTo ? e.belongTo.name : '',
             款数: e.entrySize ? e.entrySize : '',
             订单数量: e.totalQuantity ? e.totalQuantity : '',
-            服务费比例: e.serviceFeePercent ? (e.serviceFeePercent + '%') : '',
+            '服务费比例(%)': e.serviceFeePercent ? accMul(e.serviceFeePercent, 100) : '',
             订单金额: e.totalPrimeCost ? e.totalPrimeCost : '',
             结算金额: e.onlinePaidAmount ? e.onlinePaidAmount : '',
             创建时间: e.creationtime ? formatDate(new Date(e.creationtime), 'yyyy-MM-dd hh:mm:ss') : '',
             订单状态: this.getEnum('SalesProductionOrderState', e.state)
+          }
+        })
+
+        return exportData
+      },
+      async onExportByBill() {
+        const totalElements = this.page.totalElements
+
+        const exportData = await this.getExportDataByBill(totalElements)
+        exportTable(exportData, '交易流水订单')
+      },
+      async getExportDataByBill(totalElements) {
+        const query = this.queryFormData;
+
+        const url = this.apis().searchPendOut();
+        const result = await this.$http.post(url, query, {
+          page: 0,
+          size: totalElements
+        })
+
+        let exportData = [];
+        result.content.filter(item => item.paymentOrders && item.paymentOrders.length > 0).forEach(e => {
+          e.paymentOrders.filter(i => i.state === 'PAID').forEach(paymentOrder => {
+            exportData.push({
+              订单号: e.code,
+              线上支付: paymentOrder.orderType == 'offline' ? '否' : '是',
+              商户订单号: paymentOrder.code,
+              流水单号: paymentOrder.outOrderNo,
+              流水金额: paymentOrder.totalAmount ? paymentOrder.totalAmount : paymentOrder.payAmount,
+              流水支付时间: paymentOrder.paySuccessTime ? formatDate(new Date(paymentOrder.paySuccessTime),
+                'yyyy-MM-dd hh:mm:ss') : '',
+              甲方公司: e.originCooperator ? e.originCooperator.name : '',
+              乙方公司: e.belongTo ? e.belongTo.name : '',
+              款数: e.entrySize ? e.entrySize : '',
+              订单数量: e.totalQuantity ? e.totalQuantity : '',
+              '服务费比例(%)': e.serviceFeePercent ? accMul(e.serviceFeePercent, 100) : '',
+              订单金额: e.totalPrimeCost ? e.totalPrimeCost : '',
+              结算金额: e.onlinePaidAmount ? e.onlinePaidAmount : '',
+              创建时间: e.creationtime ? formatDate(new Date(e.creationtime), 'yyyy-MM-dd hh:mm:ss') : '',
+              订单状态: this.getEnum('SalesProductionOrderState', e.state)
+            })
           })
         })
-      })
 
-      return exportData
-    }
-  },
-  data () {
-    return {
-      activeName: '',
-      statuses: [
-        {
+        return exportData
+      }
+    },
+    data() {
+      return {
+        activeName: '',
+        statuses: [{
           code: '',
           name: '全部'
         }, {
@@ -166,14 +186,14 @@ export default {
         }, {
           code: 'CANCELED',
           name: '已取消'
-        }
-      ]
+        }]
+      }
+    },
+    created() {
+      this.onAdvancedSearch();
     }
-  },
-  created () {
-    this.onAdvancedSearch();
   }
-}
+
 </script>
 
 <style scoped>
