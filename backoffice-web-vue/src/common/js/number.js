d7c1e1bd3fc49e4f3a5809e5214eba65fd757386
Konoha-orz <517136675@qq.com>
2021-11-30 15:07:45 +0800

    格式化调整

--- backoffice-web-vue/src/common/js/number.js
+++ backoffice-web-vue/src/common/js/number.js
@@ -1,30 +1,28 @@
-export function accMul(arg1, arg2) {
-  if(arg1==null||arg2==null){
+export function accMul (arg1, arg2) {
+  if (arg1 == null || arg2 == null) {
     return '';
   }
   var m = 0;
   var s1 = arg1.toString();
   var s2 = arg2.toString();
   try {
-    m += s1.split(".")[1].length;
+    m += s1.split('.')[1].length;
   } catch (e) {}
   try {
-    m += s2.split(".")[1].length;
+    m += s2.split('.')[1].length;
   } catch (e) {}
-  return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+  return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m);
 }
 
-
-
-export function accAdd(arg1, arg2) {
+export function accAdd (arg1, arg2) {
   var r1, r2, m, c;
   try {
-    r1 = arg1.toString().split(".")[1].length;
+    r1 = arg1.toString().split('.')[1].length;
   } catch (e) {
     r1 = 0;
   }
   try {
-    r2 = arg2.toString().split(".")[1].length;
+    r2 = arg2.toString().split('.')[1].length;
   } catch (e) {
     r2 = 0;
   }
@@ -33,15 +31,15 @@ export function accAdd(arg1, arg2) {
   if (c > 0) {
     var cm = Math.pow(10, c);
     if (r1 > r2) {
-      arg1 = Number(arg1.toString().replace(".", ""));
-      arg2 = Number(arg2.toString().replace(".", "")) * cm;
+      arg1 = Number(arg1.toString().replace('.', ''));
+      arg2 = Number(arg2.toString().replace('.', '')) * cm;
     } else {
-      arg1 = Number(arg1.toString().replace(".", "")) * cm;
-      arg2 = Number(arg2.toString().replace(".", ""));
+      arg1 = Number(arg1.toString().replace('.', '')) * cm;
+      arg2 = Number(arg2.toString().replace('.', ''));
     }
   } else {
-    arg1 = Number(arg1.toString().replace(".", ""));
-    arg2 = Number(arg2.toString().replace(".", ""));
+    arg1 = Number(arg1.toString().replace('.', ''));
+    arg2 = Number(arg2.toString().replace('.', ''));
   }
   return (arg1 + arg2) / m;
 }
